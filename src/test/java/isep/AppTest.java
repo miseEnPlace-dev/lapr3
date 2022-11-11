eaf55b9272ff5d5b5875cc375b57fc51714febcd
Ricardo M <rmoreiradev@gmail.com>
2022-11-11 14:06:07 +0000

    updated junit

--- src/test/java/isep/AppTest.java
+++ src/test/java/isep/AppTest.java
@@ -1,13 +1,12 @@
 package isep;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
