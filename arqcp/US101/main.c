147644f66fa92af2140fe5b78412190d7cd54451
Tomás Russo <tomas.russo4@gmail.com>
2022-11-13 17:39:07 +0000

    feat(US101): modified main.c to initialize values from dev/random

--- arqcp/US101/main.c
+++ arqcp/US101/main.c
@@ -14,14 +14,25 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "random.h"
+#include "dev_random.h"		
 
 uint64_t state = 0;
 uint64_t inc = 0;
 
-int main()
-{
-  int i;
-  for (i = 0; i < 32; i++)
-    printf("%8x\n", pcg32_random_r());
-  return 0;
+int main() {
+	int i;
+
+	state = get_value_from_dev_random();
+	inc = get_value_from_dev_random();
+
+	printf("Value of state: %lu\n", state);
+	printf("Value of inc: %lu\n", inc);
+
+	printf("Values from pcg32_random_r():\n");
+
+  	for (i = 0; i < 32; i++)
+    		printf("%8x\n", pcg32_random_r());
+
+  	return 0;
 }
+
