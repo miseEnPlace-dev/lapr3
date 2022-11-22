e65954a31abf8884b7b39c7ecfc7dd4d573dc041
André Barros <andrebarros0426@gmail.com>
2022-11-22 14:53:02 +0000

    feat: added getters and setters to entity

--- src/main/java/isep/model/Entity.java
+++ src/main/java/isep/model/Entity.java
@@ -21,4 +21,71 @@ public class Entity {
   public String getLocalizationId() {
     return localizationId;
   }
+
+  /**
+   * @return String return the id
+   */
+  public String getId() {
+    return id;
+  }
+
+  /**
+   * @param id the id to set
+   */
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  /**
+   * @return double return the latitude
+   */
+  public double getLatitude() {
+    return latitude;
+  }
+
+  /**
+   * @param latitude the latitude to set
+   */
+  public void setLatitude(double latitude) {
+    this.latitude = latitude;
+  }
+
+  /**
+   * @return double return the longitude
+   */
+  public double getLongitude() {
+    return longitude;
+  }
+
+  /**
+   * @param longitude the longitude to set
+   */
+  public void setLongitude(double longitude) {
+    this.longitude = longitude;
+  }
+
+  /**
+   * @param localizationId the localizationId to set
+   */
+  public void setLocalizationId(String localizationId) {
+    this.localizationId = localizationId;
+  }
+
+  /**
+   * @return Role return the role
+   */
+  public Role getRole() {
+    return role;
+  }
+
+  /**
+   * @param role the role to set
+   */
+  public void setRole(Role role) {
+    this.role = role;
+  }
+
+  public <E extends Comparable<E>> Integer compare(Entity o1, Entity o2) {
+    return o1.getId().compareTo(o2.getId());
+  }
 }
