900e15651438203cefb4092952fd0eb15e9f2fe2
Ricardo M <rmoreiradev@gmail.com>
2022-11-23 17:43:24 +0000

    added properties util

---
+++ src/main/java/isep/shared/exceptions/MissingMandatoryPropertyException.java
@@ -0,0 +1,7 @@
+package isep.shared.exceptions;
+
+public class MissingMandatoryPropertyException extends Exception {
+  public MissingMandatoryPropertyException(String message) {
+    super(message);
+  }
+}
