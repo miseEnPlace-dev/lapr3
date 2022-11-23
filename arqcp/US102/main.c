3b3c8fbfbd82698e21558b0138f8a0ef35b6bf62
André Barros <andrebarros0426@gmail.com>
2022-11-23 13:52:28 +0000

    feat(sensors): added velc sensor

--- arqcp/US102/main.c
+++ arqcp/US102/main.c
@@ -7,9 +7,23 @@ int main(void)
 {
   printf("-- Leituras dos sensores --\n\n");
 
-  printf("Sensor de temperatura:\n");
-  for (int i = 0; i < 1000; i++) 
-  printf("Leitura: %dºC\n", sens_temp(20, i));
+  // printf("Sensor de temperatura:\n");
+  // for (int i = 0; i < 1000; i++)
+  //   printf("Leitura: %dºC\n", sens_temp(20, i));
+
+  // printf("\nSensor de velocidade vento:\n");
+  // for (int i = 0; i < 1000; i++)
+  //   printf("Leitura: %dkm\\h\n", sens_velc_vento(1, i));
+
+  // printf("\nSensor de direção vento:\n");
+  // for (int i = 0; i < 1000; i++)
+  //   printf("Leitura: %dº\n", sens_dir_vento(1, i));
+
+  printf("\nSensor de pluviosidade:\n");
+  for (char i = 0; i < 127; i++)
+    printf("Leitura: %dmm\n", sens_pluvio(1,25,i));
+
+
 
   return 0;
 }
