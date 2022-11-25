3dacebfeee7c3d269cf068547d8d632c4f92d16d
André Barros <andrebarros0426@gmail.com>
2022-11-25 22:43:25 +0000

    refactor(US102): generate base temp values

--- arqcp/US102/generate_base_temp_values.c
+++ arqcp/US102/generate_base_temp_values.c
@@ -6,23 +6,20 @@
 
 extern state;
 extern inc;
-// This function will generate a sinosoid like set of values
-void generate_base_temp_values(char* arr, int num) 
+// This function will generate a sinusoid like set of values
+void generate_base_temp_values(char* arr, int num)
 {
   state = get_value_from_dev_random();
   inc = get_value_from_dev_random();
 
   double division = M_PI / num;
-  double const STARTING_POINT_RND = pcg32_random_r();
   double const STRETCH_FATOR_RND = ((double)(pcg32_random_r() % 100) / 100)+1;
   int const BASE_TEMP_VALUE = 10;
-
-  printf("STARTING_POINT_RND=%.2f\n",STARTING_POINT_RND);
-  printf("STRETCH_FATOR_RND=%.2f\n",STRETCH_FATOR_RND);
+  int const TEMP_VARIATION = 20;
 
   for (int i = 0; i < num; i++) {
-    double angle = STRETCH_FATOR_RND*(STARTING_POINT_RND + (double)i*division);
-    char result = BASE_TEMP_VALUE+(char)(sin(angle)*10);
+    double angle = STRETCH_FATOR_RND*((double)i*division);
+    char result = BASE_TEMP_VALUE+(char)(sin(angle)*TEMP_VARIATION);
     if(result < 0) result = -result;
     *(arr+i) = result;
   }
