ad92a97f890f86c40865b10701c43dc6bb7b7f08
carlos <1211277@isep.ipp.pt>
2022-11-25 15:22:18 +0000

    chore: rename and javadoc getDistanceBetween method

--- src/main/java/isep/model/DistributionNetwork.java
+++ src/main/java/isep/model/DistributionNetwork.java
@@ -10,7 +10,15 @@ public class DistributionNetwork {
     return network.addEdge(e1, e2, distance);
   }
 
-  public Integer getDistanceBetween(Entity e1, Entity e2) {
+
+  
+  /** 
+   * @param e1 entity 1
+   * @param e2 entity 2
+   * @return Integer - If e1 and e2 are directed connected,
+   *  returns distance between. if they're not, returns null
+   */
+  public Integer getDistanceBetweenDirectedConnected(Entity e1, Entity e2) {
     if (network.edge(e1, e2) != null)
       return network.edge(e1, e2).getWeight();
     return null;
