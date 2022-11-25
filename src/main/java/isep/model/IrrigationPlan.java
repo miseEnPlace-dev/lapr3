4a4cd13b55b52171db5c526ba3a71d37acb9702d
Ricardo M <rmoreiradev@gmail.com>
2022-11-25 15:14:01 +0000

    feat: added ability to compare two irrigation plans

--- src/main/java/isep/model/IrrigationPlan.java
+++ src/main/java/isep/model/IrrigationPlan.java
@@ -31,6 +31,10 @@ public class IrrigationPlan {
     return creationDate;
   }
 
+  public int getPlanDuration() {
+    return planDuration;
+  }
+
   public CurrentIrrigationWrapper getIrrigationStatus(Calendar date) {
     // check if the plan duration is not exceeded
     Calendar tmp = (Calendar) date.clone();
@@ -40,11 +44,13 @@ public class IrrigationPlan {
     tmp.set(Calendar.MILLISECOND, 0);
 
     tmp.add(Calendar.DAY_OF_MONTH, -planDuration);
-    if (creationDate.after(tmp)) return null;
+    if (creationDate.after(tmp))
+      return null;
     tmp.add(Calendar.DAY_OF_MONTH, planDuration);
 
     // get difference of days between creationDate and date for regularity
-    int diff = (int) ((date.getTimeInMillis() - creationDate.getTimeInMillis()) / (1000 * 60 * 60 * 24));
+    int diff =
+        (int) ((date.getTimeInMillis() - creationDate.getTimeInMillis()) / (1000 * 60 * 60 * 24));
 
     // create an hour for the current date to be checked
     Hour hour = null;
@@ -59,7 +65,8 @@ public class IrrigationPlan {
     // iterate through the parcels
     for (ParcelIrrigationWrapper wrapper : parcelIrrigations) {
       // check regularity
-      if (wrapper.getRegularity().check(diff)) break;
+      if (wrapper.getRegularity().check(diff))
+        break;
 
       for (Hour cycleHour : hours) {
         // cycle hour: 8h30
@@ -67,7 +74,8 @@ public class IrrigationPlan {
         // duration offset: 8
         int fixedOffset = cycleHour.getTimeInMinutes() + offset; // 8h30 + 8min = 8h38
 
-        if (fixedOffset >= hour.getTimeInMinutes() && wrapper.getDuration() + fixedOffset < hour.getTimeInMinutes()) {
+        if (fixedOffset >= hour.getTimeInMinutes()
+            && wrapper.getDuration() + fixedOffset < hour.getTimeInMinutes()) {
           int timeRemaining = wrapper.getDuration() + fixedOffset - hour.getTimeInMinutes();
           return new CurrentIrrigationWrapper(wrapper.getParcel(), timeRemaining);
         }
@@ -77,5 +85,25 @@ public class IrrigationPlan {
     }
 
     return null;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null)
+      return false;
+    if (obj == this)
+      return true;
+    if (!(obj instanceof IrrigationPlan))
+      return false;
+
+    IrrigationPlan other = (IrrigationPlan) obj;
+
+    return other.getPlanDuration() == this.getPlanDuration()
+        && other.getParcelIrrigations().equals(this.getParcelIrrigations())
+        && other.getHours().equals(this.getHours())
+        && other.getCreationDate().get(Calendar.YEAR) == this.getCreationDate().get(Calendar.YEAR)
+        && other.getCreationDate().get(Calendar.MONTH) == this.getCreationDate().get(Calendar.MONTH)
+        && other.getCreationDate().get(Calendar.DAY_OF_MONTH) == this.getCreationDate()
+            .get(Calendar.DAY_OF_MONTH);
   }
 }
