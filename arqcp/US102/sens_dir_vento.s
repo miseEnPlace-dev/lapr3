d98e15302930316c7bbd36404adee9f6bd159a00
André Barros <andrebarros0426@gmail.com>
2022-11-26 20:06:46 +0000

    fix: fixed dir sensor only increasing numbers

--- arqcp/US102/sens_dir_vento.s
+++ arqcp/US102/sens_dir_vento.s
@@ -1,5 +1,5 @@
 .section .text
-  .global sens_dir_vento # char sens_dir_vento(unsigned short ult_dir_vento, short comp_rand)
+  .global sens_dir_vento # unsigned char sens_dir_vento(unsigned short ult_dir_vento, short comp_rand)
 
 # rdi ult_dir_vento
 # rsi comp_rand
@@ -12,21 +12,16 @@ sens_dir_vento:
   testw %ax, %ax
   je compRandZero
 
-  testw %ax, %ax
-  js compRandNeg
-
-continue:
-
   movb VELC_SENSOR_DIR_WIND_MAX_VARIATION (%rip), %cl # cl = max variation
   incb %cl # cl = max variation + 1
 
-  divb %cl # divide random component by max variation (remainder in %ah)
+  idivb %cl # divide random component by max variation (remainder in %ah)
 
   shrw $8, %ax # get the value to right position (%al)
 
   addb %dil, %al # add to last random value
 
-  jmp end
+  jmp continue
 
 compRandZero:
 
@@ -34,8 +29,13 @@ compRandZero:
   jmp end
 
 compRandNeg:
+
   negw %ax # make positive
-  jmp continue
+  jmp end
+
+continue:
+  testb %al, %al
+  js compRandNeg
 
 end:
 ret
