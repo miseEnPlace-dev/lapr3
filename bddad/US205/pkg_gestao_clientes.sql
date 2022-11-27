b88d4ab45b5104f516045e55b89d27013fd47ed4
Tomás Lopes <tomas050302@gmail.com>
2022-11-27 19:04:40 +0000

    feat: added create cliente script

--- bddad/US205/pkg_gestao_clientes.sql
+++ bddad/US205/pkg_gestao_clientes.sql
@@ -1,10 +1,16 @@
 CREATE OR REPLACE PACKAGE gestao_clientes AS
   TYPE clientes IS TABLE OF NUMBER INDEX BY BINARY_INTEGER;
 
-FUNCTION registar_cliente(id_cliente CLIENTE.id_cliente%TYPE,
-    nome CLIENTE.id_cliente,
-    morada_entrega ENCOMENDA.morada_entrega%TYPE,
-    cod_postal_entrega ENCOMENDA.cod_postal_entrega%TYPE)
-  RETURN ENCOMENDA.id_encomenda%TYPE;
+FUNCTION registar_cliente(
+    id_cliente CLIENTE.id_cliente%TYPE,
+    nome CLIENTE.nome,
+    nif CLIENTE.nif%TYPE,
+    email CLIENTE.email%TYPE,
+    morada CLIENTE.morada%TYPE,
+    morada_entrega CLIENTE.morada_entrega%TYPE,
+    plafond CLIENTE.plafond%TYPE,
+    cod_postal CLIENTE.cod_postal%TYPE,
+    cod_postal_entrega CLIENTE.cod_postal_entrega%TYPE)
+  RETURN Cliente.id_encomenda%TYPE;
 
 END gestao_clientes;
