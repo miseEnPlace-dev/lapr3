5ee16150a1e110a182c0fc596d483c88c45f2cdb
André Barros <andrebarros0426@gmail.com>
2022-11-30 17:02:36 +0000

    feat: cast data from sensors to solve problem data saving matrix

--- arqcp/US102/main.c
+++ arqcp/US102/main.c
@@ -27,14 +27,14 @@ int main(void)
 
   char base_temperatures[NUM_TEMPERATURE_REGISTERS];
 
-  char temperatures[NUM_TEMPERATURE_REGISTERS];
-  unsigned char vel_wind[NUM_VEL_WIND_REGISTERS];
-  unsigned short dir_wind[NUM_DIR_WIND_REGISTERS];
-  unsigned char pluvio[NUM_PLUVIO_REGISTERS];
-  unsigned char soil_humidity[NUM_SOIL_HUMIDITY_REGISTERS];
-  unsigned char air_humidity[NUM_AIR_HUMIDITY_REGISTERS];
+  int temperatures[NUM_TEMPERATURE_REGISTERS];
+  int vel_wind[NUM_VEL_WIND_REGISTERS];
+  int dir_wind[NUM_DIR_WIND_REGISTERS];
+  int pluvio[NUM_PLUVIO_REGISTERS];
+  int soil_humidity[NUM_SOIL_HUMIDITY_REGISTERS];
+  int air_humidity[NUM_AIR_HUMIDITY_REGISTERS];
 
-  short *matrix[NUM_OF_SENSORS];
+  int *matrix[NUM_OF_SENSORS];
   matrix[0] = temperatures;
   matrix[1] = vel_wind;
   matrix[2] = dir_wind;
@@ -49,21 +49,21 @@ int main(void)
   {
     last_temp_read = sens_temp(last_temp_read, pcg32_random_r());
     char base_temp_read = (i == 0 ? TEMP_BASE_VALUE : base_temperatures[i - 1]);
-    temperatures[i] = (last_temp_read + base_temp_read) / 2;
+    temperatures[i] =(int) ((last_temp_read + base_temp_read) / 2);
   }
 
-  unsigned char last_read = pcg32_random_r() % 50;
+  unsigned char last_read = pcg32_random_r() % 30;
   for (int i = 0; i < NUM_VEL_WIND_REGISTERS; i++)
   {
     last_read = sens_velc_vento(last_read, pcg32_random_r());
-    vel_wind[i] = last_read;
+    vel_wind[i] =(int) last_read;
   }
 
   unsigned short last_read_wind = pcg32_random_r() % 360;
   for (int i = 0; i < NUM_DIR_WIND_REGISTERS; i++)
   {
     last_read_wind = sens_dir_vento(last_read_wind, pcg32_random_r());
-    dir_wind[i] = last_read_wind;
+    dir_wind[i] =(int) last_read_wind;
   }
 
   last_read = pcg32_random_r() % 5;
@@ -71,7 +71,7 @@ int main(void)
   {
     unsigned char last_temp_read = temperatures[i * (TEMPERATURES_SENSOR_INTERVAL / PLUVIO_SENSOR_INTERVAL)];
     last_read = sens_pluvio(last_read, last_temp_read, pcg32_random_r());
-    pluvio[i] = last_read;
+    pluvio[i] =(int) last_read;
   }
 
   last_read = 10;
@@ -79,7 +79,7 @@ int main(void)
   {
     unsigned char last_pluvio_read = pluvio[i * (NUM_PLUVIO_REGISTERS / NUM_SOIL_HUMIDITY_REGISTERS)];
     last_read = sens_humd_solo(last_read, last_pluvio_read, pcg32_random_r());
-    soil_humidity[i] = last_read;
+    soil_humidity[i] =(int) last_read;
   }
 
   last_read = 10;
@@ -87,21 +87,21 @@ int main(void)
   {
     unsigned char last_pluvio_read = pluvio[i * (NUM_PLUVIO_REGISTERS / NUM_SOIL_HUMIDITY_REGISTERS)];
     last_read = sens_humd_atm(last_read, last_pluvio_read, pcg32_random_r());
-    air_humidity[i] = last_read;
+    air_humidity[i] =(int) last_read;
   }
 
   printf("-- Leituras dos sensores --\n\n");
   print_result(temperatures, NUM_TEMPERATURE_REGISTERS, "Temperatura", "ºC");
   printf("\n");
-  print_unsigned_result(vel_wind, NUM_VEL_WIND_REGISTERS, "Velocidade do Vento", "km/h");
+  print_result(vel_wind, NUM_VEL_WIND_REGISTERS, "Velocidade do Vento", "km/h");
   printf("\n");
-  print_unsigned_result_short(dir_wind, NUM_DIR_WIND_REGISTERS, "Direção do Vento", "º");
+  print_result(dir_wind, NUM_DIR_WIND_REGISTERS, "Direção do Vento", "º");
   printf("\n");
-  print_unsigned_result(pluvio, NUM_PLUVIO_REGISTERS, "Pluviosidade", "mm");
+  print_result(pluvio, NUM_PLUVIO_REGISTERS, "Pluviosidade", "mm");
   printf("\n");
-  print_unsigned_result(soil_humidity, NUM_SOIL_HUMIDITY_REGISTERS, "Humidade do Solo", "%");
+  print_result(soil_humidity, NUM_SOIL_HUMIDITY_REGISTERS, "Humidade do Solo", "%");
   printf("\n");
-  print_unsigned_result(air_humidity, NUM_AIR_HUMIDITY_REGISTERS, "Humidade do Ar", "%");
+  print_result(air_humidity, NUM_AIR_HUMIDITY_REGISTERS, "Humidade do Ar", "%");
 
   printf("\n-- Leituras dos sensores agrupadas --\n\n");
   print_result_matrix(matrix, NUM_TEMPERATURE_REGISTERS, NUM_VEL_WIND_REGISTERS, NUM_DIR_WIND_REGISTERS, NUM_PLUVIO_REGISTERS, NUM_SOIL_HUMIDITY_REGISTERS, NUM_AIR_HUMIDITY_REGISTERS);
--- arqcp/US102/print_result.c
+++ arqcp/US102/print_result.c
@@ -1,37 +1,23 @@
 #include <stdio.h>
 
-void print_result(char *arr, int size, char *name, char *units)
+void print_result(int *arr, int size, char *name, char *units)
 {
   printf("Registos %s:\n", name);
   for (int i = 0; i < size; i++)
     printf("Leitura: %d%s\n", arr[i], units);
 }
 
-void print_unsigned_result(unsigned char *arr, int size, char *name, char *units)
-{
-  printf("Registos %s:\n", name);
-  for (int i = 0; i < size; i++)
-    printf("Leitura: %u%s\n", (unsigned int)arr[i], units);
-}
-
-void print_unsigned_result_short(unsigned short *arr, int size, char *name, char *units)
-{
-  printf("Registos %s:\n", name);
-  for (int i = 0; i < size; i++)
-    printf("Leitura: %u%s\n", (unsigned int)arr[i], units);
-}
-
-void print_result_matrix(char *matrix[6], int sizeTemp, int sizeVel, int sizeDir, int sizePluvio, int sizeSoil, int sizeAir)
+void print_result_matrix(int *matrix[6], int sizeTemp, int sizeVel, int sizeDir, int sizePluvio, int sizeSoil, int sizeAir)
 {
   print_result(matrix[0], sizeTemp, "Temperatura", "ºC");
   printf("\n");
-  print_unsigned_result(matrix[1], sizeVel, "Velocidade do Vento", "m/s");
+  print_result(matrix[1], sizeVel, "Velocidade do Vento", "m/s");
   printf("\n");
-  print_unsigned_result_short(matrix[2], sizeDir, "Direção do Vento", "º");
+  print_result(matrix[2], sizeDir, "Direção do Vento", "º");
   printf("\n");
-  print_unsigned_result(matrix[3], sizePluvio, "Pluviometria", "mm");
+  print_result(matrix[3], sizePluvio, "Pluviometria", "mm");
   printf("\n");
-  print_unsigned_result(matrix[4], sizeSoil, "Humidade do Solo", "%");
+  print_result(matrix[4], sizeSoil, "Humidade do Solo", "%");
   printf("\n");
-  print_unsigned_result(matrix[5], sizeAir, "Humidade do Ar", "%");
+  print_result(matrix[5], sizeAir, "Humidade do Ar", "%");
 }
