943ad5f899c3c433216c06f0734ed8010dfbaa44
Tomás Lopes <tomas050302@gmail.com>
2022-11-30 16:13:58 +0000

    fix(database): removed generated as identity

--- bddad/US202/script.sql
+++ bddad/US202/script.sql
@@ -27,20 +27,20 @@ DROP TABLE AplicacaoFatorProducao CASCADE CONSTRAINTS PURGE;
 DROP TABLE TipoAplicacaoFatorProducao CASCADE CONSTRAINTS PURGE;
 
 CREATE TABLE Setor (
-  id_setor   number(10) GENERATED AS IDENTITY,
+  id_setor   number(10),
   designacao varchar2(50) NOT NULL,
   area       float(10) NOT NULL,
   PRIMARY KEY (id_setor)
 );
 
 CREATE TABLE EscalaoIva (
-  id_escalao_iva number(2) GENERATED AS IDENTITY,
+  id_escalao_iva number(2),
   valor          double precision NOT NULL,
   PRIMARY KEY (id_escalao_iva)
 );
 
 CREATE TABLE Produto (
-  id_produto     number(8) GENERATED AS IDENTITY,
+  id_produto     number(8),
   designacao     varchar2(50) NOT NULL,
   preco          double precision NOT NULL,
   id_escalao_iva number(2) NOT NULL,
@@ -49,13 +49,13 @@ CREATE TABLE Produto (
 );
 
 CREATE TABLE TipoCultura (
-  id_tipo_cultura number(2) GENERATED AS IDENTITY,
+  id_tipo_cultura number(2),
   tipo_cultura    varchar2(50) NOT NULL,
   PRIMARY KEY (id_tipo_cultura)
 );
 
 CREATE TABLE Cultura (
-  id_cultura      number(2) GENERATED AS IDENTITY,
+  id_cultura      number(2),
   cultura         varchar2(50) NOT NULL,
   id_tipo_cultura number(2) NOT NULL,
   id_produto      number(8) NOT NULL,
@@ -65,25 +65,25 @@ CREATE TABLE Cultura (
 );
 
 CREATE TABLE TipoFatorProducao (
-  id_tipo_fator_producao number(2) GENERATED AS IDENTITY,
+  id_tipo_fator_producao number(2),
   tipo_fator_producao    varchar2(50) NOT NULL,
   PRIMARY KEY (id_tipo_fator_producao)
 );
 
 CREATE TABLE Substancia (
-  id_substancia number(2) GENERATED AS IDENTITY,
+  id_substancia number(2),
   substancia    varchar2(50) NOT NULL,
   PRIMARY KEY (id_substancia)
 );
 
 CREATE TABLE TipoFormulacao (
-  id_tipo_formulacao number(2) GENERATED AS IDENTITY,
+  id_tipo_formulacao number(2),
   tipo_formulacao    varchar2(50) NOT NULL,
   PRIMARY KEY (id_tipo_formulacao)
 );
 
 CREATE TABLE FatorProducao (
-  id_fator_producao      number(2) GENERATED AS IDENTITY,
+  id_fator_producao      number(2),
   id_tipo_fator_producao number(2) NOT NULL,
   nome                   varchar2(50) NOT NULL,
   id_tipo_formulacao     number(2) NOT NULL,
@@ -102,13 +102,13 @@ CREATE TABLE FatorProducaoSubstancia (
 );
 
 CREATE TABLE TipoTubagem (
-  id_tipo_tubagem number(10) GENERATED AS IDENTITY,
+  id_tipo_tubagem number(10),
   tipo_tubagem    varchar2(50) NOT NULL,
   PRIMARY KEY (id_tipo_tubagem)
 );
 
 CREATE TABLE TipoRega (
-  id_tpo_rega     number(2) GENERATED AS IDENTITY,
+  id_tpo_rega     number(2),
   id_tipo_tubagem number(10) NOT NULL,
   PRIMARY KEY (id_tpo_rega),
   FOREIGN KEY (id_tipo_tubagem) REFERENCES TipoTubagem (id_tipo_tubagem) ON DELETE CASCADE
@@ -127,13 +127,13 @@ CREATE TABLE PlanoRega (
 );
 
 CREATE TABLE TipoSensor (
-  id_tipo_sensor number(2) GENERATED AS IDENTITY,
+  id_tipo_sensor number(2),
   tipo_sensor    varchar2(2) NOT NULL,
   PRIMARY KEY (id_tipo_sensor)
 );
 
 CREATE TABLE Sensor (
-  id_sensor        number(8) GENERATED AS IDENTITY,
+  id_sensor        number(8),
   identificador    varchar2(5) NOT NULL,
   id_tipo_sensor   number(2) NOT NULL,
   valor_referencia double precision NOT NULL,
@@ -149,7 +149,7 @@ CREATE TABLE Localidade (
 );
 
 CREATE TABLE Cliente (
-  id_cliente         number(8) GENERATED AS IDENTITY,
+  id_cliente         number(8),
   nome               varchar2(50) NOT NULL,
   nif                number(9) NOT NULL,
   email              varchar2(250) NOT NULL,
@@ -158,15 +158,15 @@ CREATE TABLE Cliente (
   plafond            double precision NOT NULL,
   cod_postal_entrega varchar2(8) NOT NULL,
   cod_postal         varchar2(8) NOT NULL,
-  n_encomendas           number(3) DEFAULT 0 NOT NULL, 
-  valor_total_encomendas number(10) DEFAULT 0 NOT NULL, 
+  n_encomendas           number(3) DEFAULT 0 NOT NULL,
+  valor_total_encomendas number(10) DEFAULT 0 NOT NULL,
   PRIMARY KEY (id_cliente),
   FOREIGN KEY (cod_postal_entrega) REFERENCES Localidade (cod_postal) ON DELETE CASCADE,
   FOREIGN KEY (cod_postal) REFERENCES Localidade (cod_postal) ON DELETE CASCADE
 );
 
 CREATE TABLE Encomenda (
-  id_encomenda              number(8) GENERATED AS IDENTITY,
+  id_encomenda              number(8),
   id_cliente                number(8) NOT NULL,
   data_vencimento_pagamento timestamp(0) NOT NULL,
   data_registo              timestamp(0) NOT NULL,
@@ -230,20 +230,20 @@ CREATE TABLE Plantacao (
 );
 
 CREATE TABLE TipoEdificio (
-  id_tipo_edificio number(3) GENERATED AS IDENTITY,
+  id_tipo_edificio number(3),
   tipo_edificio    varchar2(50) NOT NULL,
   PRIMARY KEY (id_tipo_edificio)
 );
 
 CREATE TABLE Edificio (
-  id_edificio      number(10) GENERATED AS IDENTITY,
+  id_edificio      number(10),
   id_tipo_edificio number(3) NOT NULL,
   PRIMARY KEY (id_edificio),
   FOREIGN KEY (id_tipo_edificio) REFERENCES TipoEdificio (id_tipo_edificio) ON DELETE CASCADE
 );
 
 CREATE TABLE TipoAplicacaoFatorProducao (
-  id_tipo_aplicacao_fator_producao number(3) GENERATED AS IDENTITY,
+  id_tipo_aplicacao_fator_producao number(3),
   tipo_aplicacao_fator_producao    varchar2(50) NOT NULL,
   PRIMARY KEY (id_tipo_aplicacao_fator_producao)
 );
@@ -260,8 +260,8 @@ CREATE TABLE AplicacaoFatorProducao (
 );
 
 CREATE TABLE ProdutoCultura (
-  id_cultura number(8) NOT NULL, 
-  id_produto number(8) NOT NULL, 
+  id_cultura number(8) NOT NULL,
+  id_produto number(8) NOT NULL,
   PRIMARY KEY (id_cultura, id_produto),
   FOREIGN KEY (id_cultura) REFERENCES Cultura (id_cultura) ON DELETE CASCADE,
   FOREIGN KEY (id_produto) REFERENCES Produto (id_produto) ON DELETE CASCADE
