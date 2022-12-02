bc66612dc70b281652463540e727d68b40d0254d
Ricardo M <rmoreiradev@gmail.com>
2022-12-02 19:33:05 +0000

    feat: finished list parcels by production qty

--- bddad/US207/pkg_rentabilidade.sql
+++ bddad/US207/pkg_rentabilidade.sql
@@ -1,4 +1,3 @@
 CREATE OR REPLACE PACKAGE rentabilidade AS
-  TYPE ListaSetores IS TABLE OF Setor%ROWTYPE;
-  FUNCTION listarSetoresPorQtdProducao RETURN ListaSetores;
+  PROCEDURE listarSetoresPorQtdProducao;
 END rentabilidade;
--- bddad/US207/pkgbody_rentabilidade.sql
+++ bddad/US207/pkgbody_rentabilidade.sql
@@ -1,17 +1,44 @@
 CREATE OR REPLACE PACKAGE BODY rentabilidade AS
-  FUNCTION listarSetoresPorQtdProducao()
-  RETURN ListaSetores IS
-    setores ListaSetores;
+  PROCEDURE listarSetoresPorQtdProducao IS
+    CURSOR setores IS
+      SELECT s.id_setor, s.designacao, s.area, SUM(c.quantidade) AS qtd_producao
+      FROM Setor s
+      INNER JOIN Colheita c
+        ON s.id_setor = c.id_setor
+      GROUP BY s.id_setor, s.designacao, s.area
+      ORDER BY SUM(c.quantidade) / s.area DESC;
+    v_id_setor SETOR.id_setor%TYPE;
+    v_designacao SETOR.designacao%TYPE;
+    v_area SETOR.area%TYPE;
+    v_qtd_producao COLHEITA.quantidade%TYPE;
+    resultado SYS_REFCURSOR;
   BEGIN
-    SAVEPOINT inicio;
+    OPEN setores;
+    FETCH setores INTO v_id_setor, v_designacao, v_area, v_qtd_producao;
 
-    SELECT s.id_setor, s.designacao, sum(c.quantidade) AS qtd_producao
-    FROM Setor s
-    INNER JOIN Colheita c
-        ON c.id_setor = s.id_setor
-    GROUP BY s.id_setor, s.designacao
-    ORDER BY qtd_producao DESC;
+    IF setores%NOTFOUND THEN
+      DBMS_OUTPUT.PUT_LINE('Não existem setores com colheitas efetuadas.');
+    ELSE
+      DBMS_OUTPUT.PUT_LINE('Setores por ordem de Quantidade de Produção:');
 
-    RETURN setores;
-  END GetSetores;
+      LOOP
+        EXIT WHEN setores%NOTFOUND;
+
+        DBMS_OUTPUT.PUT_LINE('---');
+
+        DBMS_OUTPUT.PUT_LINE('Setor ' || v_designacao || ' com id ' || v_id_setor);
+        DBMS_OUTPUT.PUT_LINE('Área: ' || v_area || ' ha');
+        DBMS_OUTPUT.PUT_LINE('Quantidade de produção: ' || TRUNC(v_qtd_producao / v_area, 4) || ' toneladas/ha');
+
+        FETCH setores INTO v_id_setor, v_designacao, v_area, v_qtd_producao;
+
+      END LOOP;
+    END IF;
+
+    CLOSE setores;
+
+  END listarSetoresPorQtdProducao;
+
+  -- PROCEDURE listarSetoresPorLucro IS
+  -- END listarSetoresPorLucro;
 END rentabilidade;
