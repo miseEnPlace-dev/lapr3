f9a9c441f406f9413baad060ab3505a53f8114db
André Barros <andrebarros0426@gmail.com>
2022-12-02 23:52:02 +0000

    fix: small fix demoRisco and fn_risco_cliente

--- bddad/US205/demoRisco.sql
+++ bddad/US205/demoRisco.sql
@@ -1,10 +1,13 @@
 DECLARE
 id_cliente1 CLIENTE.id_cliente%TYPE;
 id_cliente2 CLIENTE.id_cliente%TYPE;
+id_cliente3 CLIENTE.id_cliente%TYPE;
 n_encomendas_pendentes1 NUMBER;
 n_encomendas_pendentes2 NUMBER;
+n_encomendas_pendentes3 NUMBER;
 valor_total_incidentes1 NUMBER;
 valor_total_incidentes2 NUMBER;
+valor_total_incidentes3 NUMBER;
 
 BEGIN
 DELETE FROM encomenda;
@@ -22,9 +25,14 @@ INSERT INTO localidade VALUES ('4400-001', 'Porto');
   VALUES (2, 'Maria', '123456789', 'maria@gmail.com', 'Rua da Maria', 'Rua da Maria de Entrega', 2, '4400-001', '4400-001')
   RETURNING id_cliente INTO id_cliente2;
 
-  INSERT INTO Encomenda (id_encomenda,id_cliente,data_vencimento_pagamento,data_registo,data_entrega,data_pagamento,morada_entrega,cod_postal_entrega) VALUES (1,1,'10-Jan-2022','10-Jan-2020','20-Jan-2500',NULL,'Rua do João de Entrega','4400-001');
+  INSERT INTO cliente (id_cliente, nome, nif, email, morada, morada_entrega, plafond, cod_postal_entrega, cod_postal) VALUES (3, 'Pedro', '123456789', 'pedro@gmail.com', 'Rua do Pedro', 'Rua do Pedro de Entrega', 3, '4400-001', '4400-001')
+  RETURNING id_cliente INTO id_cliente3;
 
-  INSERT INTO Encomenda (id_encomenda,id_cliente,data_vencimento_pagamento,data_registo,data_entrega,data_pagamento,morada_entrega,cod_postal_entrega) VALUES (2,2,'20-Jan-2022','10-Jan-2020','20-Jan-2500',NULL,'Rua da Maria de Entrega','4400-001');
+  INSERT INTO Encomenda (id_encomenda,id_cliente,data_vencimento_pagamento,data_registo,data_entrega,data_pagamento,morada_entrega,cod_postal_entrega) VALUES (1,1,'10-Jan-2022','10-Jan-2020','20-Jan-2020',NULL,'Rua do João de Entrega','4400-001');
+
+  INSERT INTO Encomenda (id_encomenda,id_cliente,data_vencimento_pagamento,data_registo,data_entrega,data_pagamento,morada_entrega,cod_postal_entrega) VALUES (2,2,'20-Jan-2022','10-Jan-2020','20-Jan-2020',NULL,'Rua da Maria de Entrega','4400-001');
+
+  INSERT INTO Encomenda (id_encomenda,id_cliente,data_vencimento_pagamento,data_registo,data_entrega,data_pagamento,morada_entrega,cod_postal_entrega) VALUES (3,3,'30-Jan-2022','10-Jan-2020','20-Jan-2020','15-Jan-2020','Rua do Pedro de Entrega','4400-001');
 
   INSERT INTO ProdutoEncomenda (id_produto,id_encomenda,preco_unitario,quantidade,iva,designacao_produto) VALUES (1,1,10,10,23,'produto 1');
 
@@ -34,9 +42,11 @@ INSERT INTO localidade VALUES ('4400-001', 'Porto');
 
   SELECT COUNT(*) INTO n_encomendas_pendentes2 FROM Encomenda WHERE (id_cliente2 = id_cliente2 AND data_pagamento IS NULL);
 
-  SELECT SUM((preco_unitario * (1 + iva / 100)) * quantidade) INTO valor_total_incidentes1 FROM produtoEncomenda INNER JOIN encomenda ON produtoEncomenda.id_encomenda = encomenda.id_encomenda WHERE id_cliente = cliente_id AND (data_pagamento > data_vencimento_pagamento OR (data_vencimento_pagamento < SYSDATE AND data_pagamento IS NULL));
+  SELECT COUNT(*) INTO n_encomendas_pendentes3 FROM Encomenda WHERE (id_cliente3 = id_cliente3 AND data_pagamento IS NULL);
 
-  SELECT SUM((preco_unitario * (1 + iva / 100)) * quantidade) INTO valor_total_incidentes2 FROM produtoEncomenda INNER JOIN encomenda ON produtoEncomenda.id_encomenda = encomenda.id_encomenda WHERE id_cliente = cliente_id AND (data_pagamento > data_vencimento_pagamento OR (data_vencimento_pagamento < SYSDATE AND data_pagamento IS NULL));
+  SELECT SUM((preco_unitario * (1 + iva / 100)) * quantidade) INTO valor_total_incidentes1 FROM produtoEncomenda INNER JOIN encomenda ON produtoEncomenda.id_encomenda = encomenda.id_encomenda WHERE id_cliente = id_cliente1 AND (data_pagamento > data_vencimento_pagamento OR (data_vencimento_pagamento < SYSDATE AND data_pagamento IS NULL)) GROUP BY id_cliente;
+
+  SELECT SUM((preco_unitario * (1 + iva / 100)) * quantidade) INTO valor_total_incidentes2 FROM produtoEncomenda INNER JOIN encomenda ON produtoEncomenda.id_encomenda = encomenda.id_encomenda WHERE id_cliente = id_cliente2 AND (data_pagamento > data_vencimento_pagamento OR (data_vencimento_pagamento < SYSDATE AND data_pagamento IS NULL)) GROUP BY id_cliente;
 
   DBMS_OUTPUT.PUT_LINE('Encomendas Pendentes do Cliente 1: ' || n_encomendas_pendentes1);
 
@@ -51,6 +61,8 @@ INSERT INTO localidade VALUES ('4400-001', 'Porto');
   DBMS_OUTPUT.PUT_LINE(chr(13)||chr(10));
 
   DBMS_OUTPUT.PUT_LINE('Valor Total Incidentes do Cliente 2: ' || valor_total_incidentes2);
+
+  DBMS_OUTPUT.PUT_LINE(chr(13)||chr(10));
 
   DBMS_OUTPUT.PUT_LINE('Risco do cliente 1: ' || gestao_clientes.fn_risco_cliente(id_cliente1));
 
