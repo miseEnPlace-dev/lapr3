b9540c3204296a6ab6b0ac36bb18aa781efe9a3c
carlos <1211277@isep.ipp.pt>
2022-12-04 21:36:35 +0000

    feat(db): test exceptions raise

--- bddad/US206/demo.sql
+++ bddad/US206/demo.sql
@@ -19,9 +19,9 @@ BEGIN
   id_tipo_cultura1 := gestao_setores.fn_RegistarTipoCultura('Tipo 2');
   id_tipo_cultura2 := gestao_setores.fn_RegistarTipoCultura('Tipo 7');
   id_tipo_cultura3 := gestao_setores.fn_RegistarTipoCultura('Tipo 5');
-  id_cultura1 := gestao_setores.fn_RegistarCultura('Cultura 4', id_setor1);
-  id_cultura2 := gestao_setores.fn_RegistarCultura('Cultura 5', id_setor2);
-  id_cultura3 := gestao_setores.fn_RegistarCultura('Cultura 1', id_setor3);
+  id_cultura1 := gestao_setores.fn_RegistarCultura('Cultura 4', id_tipo_cultura1);
+  id_cultura2 := gestao_setores.fn_RegistarCultura('Cultura 5', id_tipo_cultura2);
+  id_cultura3 := gestao_setores.fn_RegistarCultura('Cultura 1', id_tipo_cultura3);
   gestao_setores.pr_RegistarPlantacao(id_setor1, id_cultura1, '01-Jan-2020 12:00:00');
   gestao_setores.pr_RegistarPlantacao(id_setor2, id_cultura2, '01-Jan-2020 12:00:00');
   gestao_setores.pr_RegistarPlantacao(id_setor3, id_cultura3, '01-Jan-2020 12:00:00');
@@ -32,5 +32,19 @@ BEGIN
   gestao_setores.pr_ListarSetoresTamanhoDecrescente;
   gestao_setores.pr_ListarSetoresOrdemTipoCultura;
   gestao_setores.pr_ListarSetoresOrdemCultura;
+
+
+  /* Test Exceptions */
+
+  DBMS_OUTPUT.PUT_LINE('Expected tipo_cultura_inexistente ');
+  id_cultura1 := gestao_setores.fn_RegistarCultura('Cultura 4', 99);
+  
+  DBMS_OUTPUT.PUT_LINE('Expected setor_inexistente');
+  gestao_setores.pr_RegistarPlantacao(99, id_cultura1, '01-Jan-2020 12:00:00');
+
+  
+  DBMS_OUTPUT.PUT_LINE('Expected cultura_inexistente');
+  gestao_setores.pr_RegistarPlantacao(id_setor1, 99, '01-Jan-2020 12:00:00');
+
 
 END;
