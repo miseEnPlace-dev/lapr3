75e36b36093653be2d1a5aa3f221ee92449d36b5
carlos <1211277@isep.ipp.pt>
2022-12-15 11:53:58 +0000

    chore(db): listar logs output

--- bddad/US213/pkgbody_listar_logs.sql
+++ bddad/US213/pkgbody_listar_logs.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE PACKAGE BODY listar_logs AS
     CURSOR all_logs IS
       SELECT id_log, utilizador, tipo_alteracao, data_alteracao, tabela
       FROM LOGS
-      ORDER BY data_alteracao;
+      ORDER BY data_alteracao, id_log;
     log_id LOGS.id_log%TYPE;
     utilizador LOGS.utilizador%TYPE;
     alteracao LOGS.tipo_alteracao%TYPE;
@@ -17,14 +17,14 @@ CREATE OR REPLACE PACKAGE BODY listar_logs AS
     IF all_logs%ROWCOUNT = 0 THEN
       DBMS_OUTPUT.PUT_LINE('Não existem logs registadas.');
     ELSE
-      DBMS_OUTPUT.PUT_LINE('Logs:');
+      DBMS_OUTPUT.PUT_LINE('Logs ordenadas :');
       DBMS_OUTPUT.PUT_LINE('');
 
       LOOP
         EXIT WHEN all_logs%NOTFOUND;
 
         DBMS_OUTPUT.PUT_LINE('----');
-        DBMS_OUTPUT.PUT_LINE('Log id: ' || log_id || '. Utilizador ' || utilizador || ' realizou ' || alteracao || ' na tabela ' || tab || ' a ' || TO_CHAR(data_hora, 'DD/MM/YYYY') || '.');
+        DBMS_OUTPUT.PUT_LINE('Log id: ' || log_id || '. Utilizador ' || utilizador || ' realizou ' || alteracao || ' na tabela ' || tab || ' a ' || TO_CHAR(data_hora, 'DD/MM/YYYY HH24:MI:SS') || '.');
 
         FETCH all_logs INTO log_id, utilizador, alteracao, data_hora, tab;
       END LOOP;
