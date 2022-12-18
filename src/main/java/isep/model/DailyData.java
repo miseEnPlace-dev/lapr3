11b81971027a15295f79e79a508902bd8f55690a
carlos <1211277@isep.ipp.pt>
2022-12-18 14:17:41 +0000

    feat: DailyData class

---
+++ src/main/java/isep/model/DailyData.java
@@ -0,0 +1,66 @@
+package isep.model;
+
+import java.util.HashMap;
+
+public class DailyData {
+
+    private HashMap<Integer, HashMap<Product, Integer>> dailyData;
+
+    public DailyData(){
+        this.dailyData = new HashMap<>();
+    }
+
+    
+    /** 
+     * @param day - day to add data
+     * @param products - product/quantity data
+     * 
+     * Adds the products/quantity data to a specif day
+     */
+    public void addDayData(Integer day, HashMap<Product, Integer> products){
+        this.dailyData.put(day, products);
+    }
+    
+    
+    /** 
+     * @param day - day to add data
+     * @param product - product adding
+     * @param quantity - quantity of product adding
+     * 
+     * Adds a single product/quantity to a specif day
+     */
+    public void addProductInfoToDayData(Integer day, Product product, Integer quantity){
+        HashMap<Product, Integer> map;
+        map = this.dailyData.containsKey(day) ? this.dailyData.get(day) : new HashMap<>();
+
+        map.put(product, quantity);
+
+        this.dailyData.put(day, map);
+    }
+
+    
+    /** 
+     * @param day - day to get data from
+     * @return HashMap<Product, Integer>
+     * 
+     * Returns a map with the products and quantities registered 
+     * in that dailyData for a specif day
+     */
+    public HashMap<Product, Integer> getDayData(Integer day){
+        return this.dailyData.get(day);
+    }
+
+    
+    /** 
+     * @param day - day to get data from 
+     * @param product - product to get data from
+     * @return Integer
+     * 
+     * Returns the quantity of a product registered
+     * in that dailyData for a specif day
+     */
+    public Integer getQuantityOfProductForDay(Integer day, Product product){
+        return this.dailyData.get(day).get(product) != null ? this.dailyData.get(day).get(product) : 0;
+    }
+
+}
