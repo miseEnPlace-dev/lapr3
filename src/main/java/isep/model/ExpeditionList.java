d9c6b2c414fd45f9f3c83397920e8ddc53ce8670
André Barros <andrebarros0426@gmail.com>
2022-12-19 17:02:38 +0000

    feat: ExpeditionList class

---
+++ src/main/java/isep/model/ExpeditionList.java
@@ -0,0 +1,40 @@
+package isep.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpeditionList {
+
+  private List<Basket> baskets;
+  private int day;
+
+  public ExpeditionList(int day) {
+    this.baskets = new ArrayList<>();
+    setDay(day);
+  }
+
+  private void setDay(int day) {
+    if (day > 0) {
+      throw new IllegalArgumentException("Day must be positive number");
+    }
+    this.day = day;
+  }
+
+  public int getDay() {
+    return this.day;
+  }
+
+  public void addBasket(Basket basket) {
+    if (basket == null)
+      throw new IllegalArgumentException("Basket cannot be null");
+    this.baskets.add(basket);
+  }
+
+  public List<Basket> getBaskets() {
+    return this.baskets;
+  }
+
+  public int getNumberOfBaskets() {
+    return this.baskets.size();
+  }
+}
