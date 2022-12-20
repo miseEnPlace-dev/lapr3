4f39c643d716380758bb1b4b9b290d2df1fdf578
André Barros <andrebarros0426@gmail.com>
2022-12-20 16:20:50 +0000

    feat(db): added procedure to update operation with data

--- bddad/US211/pkgbody_operacoes.sql
+++ bddad/US211/pkgbody_operacoes.sql
@@ -1,8 +1,6 @@
 CREATE OR REPLACE PACKAGE BODY operacoes_nao_realizadas AS
 
   PROCEDURE cancel_operacao(operacao_id Operacao.id_operacao%TYPE) IS
-  data_prevista_op Operacao.data_prevista_operacao%TYPE;
-  data_op Operacao.data_operacao%TYPE;
 
   BEGIN
 
@@ -20,5 +18,24 @@ CREATE OR REPLACE PACKAGE BODY operacoes_nao_realizadas AS
   END IF;
 
   END cancel_operacao;
+
+  PROCEDURE atualizar_operacao_datas(operacao_id Operacao.id_operacao%TYPE, data_nova TIMESTAMP) IS
+
+  BEGIN
+
+  IF(EXISTS(
+      SELECT *
+      FROM Operacao
+      WHERE id_operacao = operacao_id
+      AND data_prevista_operacao IS NOT NULL AND data_operacao IS NULL;
+  )) THEN
+    UPDATE Operacao
+    SET data_prevista_operacao = data_nova
+    WHERE id_operacao = operacao_id;
+  ELSE
+    RAISE_APPLICATION_ERROR(-20001, 'Operação já realizada');
+  END IF;
+
+  END atualizar_operacao_datas;
 
 END operacoes_nao_realizadas;
