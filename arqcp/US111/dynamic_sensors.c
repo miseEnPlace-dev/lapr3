11f2d45bd46a28a282b81d16ccbe508c2b28751c
Ricardo M <rmoreiradev@gmail.com>
2022-12-20 16:17:44 +0000

    init us 111

---
+++ arqcp/US111/dynamic_sensors.c
@@ -0,0 +1,12 @@
+#include "sensor.h"
+
+enum { A = 1, B = 2 };
+
+void add_sensor(Sensor s) {
+
+}
+
+void remove_sensor() {}
+
+void adjust_sensor_freq(Sensor *s) {}
+
---
+++ arqcp/US111/print_result.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sensor.h"
+
+void print_result(Sensor *arr, int size, char *name, char *units, int n_of_sensors, char **errors)
+{
+  printf("-- Registos %s: --\n", name);
+  for (int j = 0; j < n_of_sensors; j++)
+  {
+    printf("\nSensor %d:\n", j + 1);
+    for (int i = 0; i < size; i++)
+      printf("Leitura: %u%s %s\n", arr[j].readings[i], units, errors[j][i] == 1 ? "(Erro)" : "");
+    free(arr[j].readings);
+  }
+
+  free(arr);
+}
+
+void print_signed_result(Sensor *arr, int size, char *name, char *units, int n_of_sensors, char **errors)
+{
+  printf("-- Registos %s: --\n", name);
+  for (int j = 0; j < n_of_sensors; j++)
+  {
+    printf("\nSensor %d:\n", j + 1);
+    for (int i = 0; i < size; i++)
+      printf("Leitura: %d%s %s\n", (char)arr[j].readings[i], units, errors[j][i] == 1 ? "(Erro)" : "");
+    free(arr[j].readings);
+  }
+  free(arr);
+}
