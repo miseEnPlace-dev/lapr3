40939c9c81deaf3c39a06792972e6d503dda43e0
André Barros <andrebarros0426@gmail.com>
2022-12-20 16:15:49 +0000

    feat(db): added new procedures to packgage operations

--- bddad/US211/pkg_operacoes.sql
+++ bddad/US211/pkg_operacoes.sql
@@ -1,6 +1,8 @@
 CREATE OR REPLACE PACKAGE operacoes_nao_realizadas AS
+  TYPE produtos IS TABLE OF NUMBER INDEX BY BINARY_INTEGER;
 
   PROCEDURE cancel_operacao (id_operacao Operacao.id_operacao%TYPE);
-  PROCEDURE atualizar_operacao (id_operacao Operacao.id_operacao%TYPE);
+  PROCEDURE atualizar_operacao_produtos (lista_produtos produtos, id_operacao Operacao.id_operacao%TYPE);
+  PROCEDURE atualizar_operacao_datas (id_operacao Operacao.id_operacao%TYPE, data_nova TIMESTAMP);
 
 END operacoes_nao_realizadas;
