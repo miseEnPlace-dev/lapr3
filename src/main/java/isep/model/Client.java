8140bb4edcd6f40deca6e35760de53d231ab6531
carlos <1211277@isep.ipp.pt>
2022-12-21 16:13:14 +0000

    feat: implemented client orders and producer stock

--- src/main/java/isep/model/Client.java
+++ src/main/java/isep/model/Client.java
@@ -1,7 +1,36 @@
 package isep.model;
 
+import java.util.HashMap;
+
 public class Client extends Entity {
+
+  DailyData orders;
+
   public Client(String id, double latitude, double longitude, String localizationId) {
     super(id, latitude, longitude, localizationId);
+    this.orders = new DailyData();
   }
+
+  public void setOrder(DailyData orders){
+    if(orders == null)
+      throw new IllegalArgumentException("Orders cannot be null!");
+    this.orders = orders;
+  }
+
+  public DailyData getOrders(){
+    return this.orders;
+  }
+
+  public void setOrder(Integer day, HashMap<Product, Integer> order){
+    if(day <= 0)
+      throw new IllegalArgumentException("Day must be a positive number!");
+    if(order == null)
+      throw new IllegalArgumentException("Order cannot be null!");
+    this.orders.addDayData(day, order);
+  }
+
+  public HashMap<Product, Integer> getOrder(Integer day){
+    return this.orders.getDayData(day);
+  }
+
 }
