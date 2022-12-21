c2be5855846f9d617e2008dce0722cca974f4c77
Ricardo M <rmoreiradev@gmail.com>
2022-12-21 16:38:48 +0000

    chore: better alignment for sensor struct (saved 8 bytes)

--- arqcp/US111/sensor.h
+++ arqcp/US111/sensor.h
@@ -2,14 +2,23 @@
 #define SENSOR_H
 typedef struct
 {
-  unsigned short id;
-  unsigned char sensor_type;
-  char *name;               // nome do sensor
-  unsigned short max_limit; // limites do sensor
-  unsigned short min_limit;
-  unsigned long frequency;     // frequency de leituras (em segundos)
-  unsigned long readings_size; // tamanho do array de leituras
-  unsigned short *readings;    // array de leituras diárias
+  char *name;                   // nome do sensor
   char *units;
+  unsigned long frequency;      // frequency de leituras (em segundos)
+  unsigned long readings_size;  // tamanho do array de leituras
+  unsigned short *readings;     // array de leituras diárias
+  unsigned char *errors;        // array de erros das leituras
+  unsigned short id;
+  unsigned short max_limit;     // limites do sensor
+  unsigned short min_limit; 
+  unsigned char sensor_type;
 } Sensor;
+
+// original:
+// ***. .... **** **** **** .... **** **** **** **** **** **** **** **** **** ****
+
+// optimized:
+// 
+
 #endif
+
