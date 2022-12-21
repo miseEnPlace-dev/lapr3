00a9317449809f94e2399569546e166ea0e6a301
André Barros <andrebarros0426@gmail.com>
2022-12-21 20:05:56 +0000

    fix: removed unnecessary struct to import from csv

--- arqcp/US110/import_from_csv.h
+++ arqcp/US110/import_from_csv.h
@@ -1,11 +1,6 @@
 #ifndef IMPORT_FROM_CSV_H
 #define IMPORT_FROM_CSV_H
 
-typedef struct {
-
-    int number_sensors;
-} sensor_n;
-
-void import_from_csv(char *filename, sensor_n *sensors);
+void import_from_csv(char *filename, Sensor *sensors);
 
 #endif
