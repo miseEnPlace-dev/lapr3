a5bb92b3498ec00996f8db7605e50a1dd365ccf3
Ricardo M <rmoreiradev@gmail.com>
2022-12-22 13:36:06 +0000

    feat(arqcp): add sensor of a certain type function

--- arqcp/US111/dynamic_sensors.c
+++ arqcp/US111/dynamic_sensors.c
@@ -1,8 +1,13 @@
+#include <stdlib.h>
+
 #include "sensor.h"
 #include "sensors.h"
 
 void add_sensor(Sensor s, Sensor **data, unsigned int *n_sensors) {
-    // keep n_sensors updated
+    Sensor *ptr = (Sensor *)realloc(data[s.sensor_type], (n_sensors[s.sensor_type] + 1) * sizeof(Sensor *));
+    if (ptr == NULL) return;
+    data[s.sensor_type] = ptr;
+    n_sensors[s.sensor_type]++; // keep n_sensors updated
 }
 
 void remove_sensor(Sensor *s, Sensor **data, unsigned int *n_sensors) {
