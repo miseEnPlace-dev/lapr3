2b22cb0033d6aced3675bdb9b0fe16a334bd774c
André Barros <andrebarros0426@gmail.com>
2022-12-22 17:29:16 +0000

    fix(db): operation cancelation

--- bddad/US211/pkgbody_operacoes.sql
+++ bddad/US211/pkgbody_operacoes.sql
@@ -1,41 +1,44 @@
-CREATE OR REPLACE PACKAGE BODY operacoes_nao_realizadas AS
+CREATE OR REPLACE PACKAGE BODY operacoes AS
 
-  PROCEDURE cancel_operacao(operacao_id Operacao.id_operacao%TYPE) IS
-
+  PROCEDURE cancel_operacao (operacao_id Operacao.id_operacao%TYPE) IS
+  data_prevista TIMESTAMP;
+  data_op TIMESTAMP;
   BEGIN
 
-  IF(EXISTS(
-      SELECT *
-      FROM Operacao
-      WHERE id_operacao = operacao_id
-      AND data_prevista_operacao IS NOT NULL AND data_operacao IS NULL;
-  )) THEN
-    UPDATE Operacao
-    SET data_prevista_operacao = NULL
+    SELECT data_prevista_operacao, data_operacao
+    INTO data_prevista, data_op
+    FROM Operacao
     WHERE id_operacao = operacao_id;
-  ELSE
-    RAISE_APPLICATION_ERROR(-20001, 'Operação já realizada');
-  END IF;
+
+    IF data_prevista IS NOT NULL AND data_op IS NOT NULL THEN
+      RAISE_APPLICATION_ERROR(-20001, 'Operação já realizada');
+    ELSE IF data_prevista IS NOT NULL AND data_op IS NULL THEN
+      UPDATE Operacao
+      SET data_prevista_operacao = NULL
+      WHERE id_operacao = operacao_id;
+    END IF;
+    END IF;
 
   END cancel_operacao;
 
   PROCEDURE atualizar_operacao_datas(operacao_id Operacao.id_operacao%TYPE, data_nova TIMESTAMP) IS
-
+  data_prevista TIMESTAMP;
+  data_op TIMESTAMP;
   BEGIN
-
-  IF(EXISTS(
-      SELECT *
-      FROM Operacao
-      WHERE id_operacao = operacao_id
-      AND data_prevista_operacao IS NOT NULL AND data_operacao IS NULL;
-  )) THEN
-    UPDATE Operacao
-    SET data_prevista_operacao = data_nova
+  SELECT data_prevista_operacao, data_operacao
+    INTO data_prevista, data_op
+    FROM Operacao
     WHERE id_operacao = operacao_id;
-  ELSE
-    RAISE_APPLICATION_ERROR(-20001, 'Operação já realizada');
-  END IF;
+
+    IF data_prevista IS NOT NULL AND data_op IS NOT NULL THEN
+      RAISE_APPLICATION_ERROR(-20001, 'Operação já realizada');
+    ELSE IF data_prevista IS NOT NULL AND data_op IS NULL THEN
+      UPDATE Operacao
+      SET data_prevista_operacao = data_nova
+      WHERE id_operacao = operacao_id;
+    END IF;
+    END IF;
 
   END atualizar_operacao_datas;
 
-END operacoes_nao_realizadas;
+END operacoes;
