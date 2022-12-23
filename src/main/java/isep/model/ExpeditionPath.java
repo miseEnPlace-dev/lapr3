92a00375ce79d121f0f28bf7bc0b235a8dee33cb
Tomás Russo <tomas.russo4@gmail.com>
2022-12-23 20:58:23 +0000

    feat: new methods to ExpeditionPath

--- src/main/java/isep/model/ExpeditionPath.java
+++ src/main/java/isep/model/ExpeditionPath.java
@@ -1,5 +1,7 @@
 package isep.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +16,7 @@ import java.util.Map;
  */
 public class ExpeditionPath {
   private DistributionNetwork distributionNetwork;
+  private ExpeditionList expeditionList;
   private List<Entity> path;
   private Map<Enterprise, Integer> deliveredBaskets;
   private int totalDistance;
@@ -24,9 +27,78 @@ public class ExpeditionPath {
    * @param distributionNetwork The distribution network
    * @param path                The expedition path
    */
-  public ExpeditionPath(DistributionNetwork distributionNetwork, List<Entity> path) {
+  public ExpeditionPath(DistributionNetwork distributionNetwork, ExpeditionList expeditionList) {
     this.distributionNetwork = distributionNetwork;
-    this.path = path;
+    this.expeditionList = expeditionList;
+    buildPath();
+  }
+
+  /**
+   * Builds the shortest path.
+   */
+  private void buildPath() {
+    path = new ArrayList<>();
+
+    // Get all producers in the expedition list
+    List<Producer> producers = expeditionList.getProducers();
+
+    // Pop (get and remove) first producer from the list
+    Entity currentEntity = producers.get(0);
+    producers.remove(currentEntity);
+
+    // Find the shortest path between all producers
+    while (!producers.isEmpty()) {
+      int minDistance = Integer.MAX_VALUE;
+      Producer minProducer = null;
+      List<Entity> minPath = null;
+
+      // Iterate over all other producers, in order to find the nearest one
+      for (int i = 0; i < producers.size(); i++) {
+        List<Entity> shortestPath = distributionNetwork.getShortestPathUsingAStar(currentEntity, producers.get(i));
+        int distance = distributionNetwork.getWeightOfPath(shortestPath);
+
+        if (distance < minDistance) {
+          minDistance = distance;
+          minProducer = producers.get(i);
+          minPath = shortestPath;
+          minPath.remove(minPath.size() - 1);
+        }
+      }
+
+      currentEntity = minProducer;
+      path.addAll(minPath);
+      producers.remove(currentEntity);
+    }
+
+    // Get all hubs in the expedition list
+    List<Enterprise> hubs = expeditionList.getHubs();
+
+    // Find the shortest path between all hubs
+    while (!hubs.isEmpty()) {
+      int minDistance = Integer.MAX_VALUE;
+      Enterprise minHub = null;
+      List<Entity> minPath = null;
+
+      // Iterate over all other hubs, in order to find the nearest one
+      for (int i = 0; i < hubs.size(); i++) {
+        List<Entity> shortestPath = distributionNetwork.getShortestPathUsingAStar(currentEntity, hubs.get(i));
+        int distance = distributionNetwork.getWeightOfPath(shortestPath);
+
+        if (distance < minDistance) {
+          minDistance = distance;
+          minHub = hubs.get(i);
+          minPath = shortestPath;
+          minPath.remove(minPath.size() - 1);
+        }
+      }
+
+      currentEntity = minHub;
+      path.addAll(minPath);
+      hubs.remove(currentEntity);
+    }
+
+    path.add(currentEntity);
+
     setTotalDistance();
   }
 
@@ -71,5 +143,37 @@ public class ExpeditionPath {
     if (path.contains(entity1) && path.contains(entity2))
       return this.distributionNetwork.getDistanceBetweenConnectedEntities(entity1, entity2);
     return -1;
+  }
+
+  /**
+   * Get the distance between all entities of the path.
+   * 
+   * @return A {@code Map} that associates a {@code Entry} of two Entities to
+   *         it's distance
+   */
+  public Map<Map.Entry<Entity, Entity>, Integer> getDistanceBetweenAllEntities() {
+    Map<Map.Entry<Entity, Entity>, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < path.size() - 1; i++) {
+      int distance = this.distributionNetwork.getDistanceBetweenConnectedEntities(path.get(i), path.get(i + 1));
+      map.put(Map.entry(path.get(i), path.get(i + 1)), distance);
+    }
+
+    return map;
+  }
+
+  /**
+   * Prints the expedition path.
+   */
+  public void printPath() {
+    System.out.println("Expedition path for day " + this.expeditionList.getDay() + ":");
+    for (int i = 0; i < path.size(); i++) {
+      String str = ">> Stop " + (i + 1) + ": ";
+      if (path.get(i).getClass() == Producer.class)
+        str = str.concat("Producer " + path.get(i).getId());
+      else
+        str = str.concat("Hub " + path.get(i).getId());
+      System.out.println(str);
+    }
   }
 }
