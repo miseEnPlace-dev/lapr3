9f59cf316f8df30a1bd0168cf7d555a74535b6af
André Barros <andrebarros0426@gmail.com>
2022-12-26 17:33:39 +0000

    feat: added view to check last five years of production

--- bddad/US214/pkg_warehouse.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE PACKAGE warehouse AS
-    FUNCTION evolucaoProducao (prod Produto.designacao%TYPE, ano Tempo.ano%TYPE, mes Tempo.mes%TYPE, setor Producao.id_setor%TYPE ) RETURN NUMBER;
-
-END warehouse;
