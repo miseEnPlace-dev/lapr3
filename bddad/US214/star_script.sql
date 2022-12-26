a8e46596ff2afe2dfe0205280a208efb0e2f5ddb
André Barros <andrebarros0426@gmail.com>
2022-12-26 12:51:15 +0000

    feat(db): Star data model script

---
+++ bddad/US214/star_script.sql
@@ -0,0 +1,65 @@
+-- Star data model --
+
+-- Dimension tables --
+CREATE TABLE Cliente (
+  id_cliente NUMBER(10,0) NOT NULL,
+  nome VARCHAR(50,0) NOT NULL,
+  nif NUMBER(9,0) NOT NULL,
+  PRIMARY KEY (id_cliente),
+  CONSTRAINT CHK_Nif    CHECK (nif > 100000000 AND nif < 999999999)
+);
+
+CREATE TABLE Produto (
+  id_produto NUMBER(10,0) NOT NULL,
+  tipo VARCHAR(50,0) NOT NULL,
+  preco NUMBER(10,2) NOT NULL,
+  PRIMARY KEY (id_produto)
+);
+
+CREATE TABLE Setor (
+  id_setor NUMBER(10,0) NOT NULL,
+  nome VARCHAR(50,0) NOT NULL,
+  PRIMARY KEY (id_setor)
+);
+
+CREATE TABLE Tempo (
+  id_tempo NUMBER(10,0) NOT NULL,
+  ano NUMBER(4,0) NOT NULL,
+  mes NUMBER(2,0) NOT NULL,
+  PRIMARY KEY (id_tempo),
+  CONSTRAINT CHK_Mes    CHECK (mes > 0 AND mes < 13)
+  CONSTRAINT CHK_Ano    CHECK (ano > 0)
+);
+
+-- Fact table --
+CREATE TABLE Venda (
+  id_venda NUMBER(10,0) NOT NULL,
+  id_cliente NUMBER(10,0) NOT NULL,
+  id_produto NUMBER(10,0) NOT NULL,
+  id_setor NUMBER(10,0) NOT NULL,
+  id_tempo NUMBER(10,0) NOT NULL,
+  quantidade NUMBER(10,0) NOT NULL,
+  PRIMARY KEY (id_venda),
+  CONSTRAINT FK_Venda_Cliente    FOREIGN KEY (id_cliente) REFERENCES Cliente (id_cliente),
+  CONSTRAINT FK_Venda_Produto    FOREIGN KEY (id_produto) REFERENCES Produto (id_produto),
+  CONSTRAINT FK_Venda_Setor    FOREIGN KEY (id_setor) REFERENCES Setor (id_setor),
+  CONSTRAINT FK_Venda_Tempo    FOREIGN KEY (id_tempo) REFERENCES Tempo (id_tempo),
+  CONSTRAINT CHK_Quantidade    CHECK (quantidade > 0)
+);
+
+CREATE TABLE Producao (
+  id_producao NUMBER (10,0) NOT NULL,
+  id_produto NUMBER(10,0) NOT NULL,
+  id_setor NUMBER(10,0) NOT NULL,
+  id_tempo NUMBER(10,0) NOT NULL,
+  quantidade NUMBER(10,0) NOT NULL,
+  PRIMARY KEY (id_producao),
+  CONSTRAINT FK_Producao_Produto    FOREIGN KEY (id_produto) REFERENCES Produto (id_produto),
+  CONSTRAINT FK_Producao_Setor    FOREIGN KEY (id_setor) REFERENCES Setor (id_setor),
+  CONSTRAINT FK_Producao_Tempo    FOREIGN KEY (id_tempo) REFERENCES Tempo (id_tempo),
+  CONSTRAINT CHK_Quantidade    CHECK (quantidade > 0)
+);
+
+
+
+
