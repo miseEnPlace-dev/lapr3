e0ea14fd8de1c60010655aef98c061c852dc6852
Tomás Russo <tomas.russo4@gmail.com>
2022-12-28 16:50:16 +0000

    feat(US210): updated pkg header

--- bddad/US210/pkg_gestao_operacoes.sql
+++ bddad/US210/pkg_gestao_operacoes.sql
@@ -7,10 +7,10 @@ CREATE OR REPLACE PACKAGE gestao_operacoes AS
   * Recebe a data prevista, o id do setor, o id do tipo de aplicação e a lista de fatores de produção.
   * Retorna o id da aplicação. */
   FUNCTION fn_registar_aplicacao_fp (
-    data_prevista OPERACAO.data_prevista_operacao%TYPE,
-    id_setor SETOR.id_setor%TYPE,
-    id_tipo_aplicacao TIPOAPLICACAO.id_tipo_aplicacao%TYPE,
-    lista_fatores_producao fatores_producao
+    p_data_prevista OPERACAO.data_prevista_operacao%TYPE,
+    p_id_setor SETOR.id_setor%TYPE,
+    p_id_tipo_aplicacao TIPOAPLICACAO.id_tipo_aplicacao%TYPE,
+    p_lista_fatores_producao fatores_producao
   ) RETURN OPERACAO.id_operacao%TYPE;
 
   /* Permite marcar uma rega.
@@ -18,9 +18,9 @@ CREATE OR REPLACE PACKAGE gestao_operacoes AS
   * Recebe a data prevista, o id do setor e o id do tipo de rega.
   * Retorna o id da rega. */
   FUNCTION fn_registar_rega (
-    data_prevista OPERACAO.data_prevista_operacao%TYPE,
-    id_setor SETOR.id_setor%TYPE,
-    id_tipo_rega TIPOREGA.id_tipo_rega%TYPE
+    p_data_prevista OPERACAO.data_prevista_operacao%TYPE,
+    p_id_setor SETOR.id_setor%TYPE,
+    p_id_tipo_rega TIPOREGA.id_tipo_rega%TYPE
   ) RETURN OPERACAO.id_operacao%TYPE;
 
   /* Permite marcar uma colheita.
@@ -28,10 +28,10 @@ CREATE OR REPLACE PACKAGE gestao_operacoes AS
   * Recebe a data prevista, o id da plantação, o id do produto e respetiva quantidade.
   * Retorna o id da colheita. */
   FUNCTION fn_registar_colheita (
-    data_prevista OPERACAO.data_prevista_operacao%TYPE,
-    id_plantacao PLANTACAO.id_plantacao%TYPE,
-    id_produto PRODUTO.id_produto%TYPE,
-    quantidade_produto COLHEITA.quantidade%TYPE
+    p_data_prevista OPERACAO.data_prevista_operacao%TYPE,
+    p_id_plantacao PLANTACAO.id_plantacao%TYPE,
+    p_id_produto PRODUTO.id_produto%TYPE,
+    p_quantidade_produto COLHEITA.quantidade%TYPE
   ) RETURN OPERACAO.id_operacao%TYPE;
 
   /* Verifica as restrições de uma aplicação de fatores de produção.
@@ -39,7 +39,7 @@ CREATE OR REPLACE PACKAGE gestao_operacoes AS
   * Recebe o id da aplicação.
   * Retorna TRUE se a aplicação é válida, FALSE caso contrário. */
   FUNCTION fn_verificar_aplicacao_fp (
-    id_operacao OPERACAO.id_operacao%TYPE
+    p_id_operacao OPERACAO.id_operacao%TYPE
   ) RETURN BOOLEAN;
 
   /* Lista as restrições de aplicação de fatores de produção num dado setor, para um dado dia.
@@ -47,8 +47,8 @@ CREATE OR REPLACE PACKAGE gestao_operacoes AS
   * Recebe o id do setor e a data.
   * Imprime a lista de restrições. */
   PROCEDURE pr_listar_restricoes_aplicacao_fp (
-    id_setor SETOR.id_setor%TYPE,
-    data DATE
+    p_id_setor SETOR.id_setor%TYPE,
+    p_data DATE
   );
 
   /* Lista as operações de um dado setor, para um dado intervalo de tempo.
@@ -56,11 +56,11 @@ CREATE OR REPLACE PACKAGE gestao_operacoes AS
   * Recebe o id do setor, a data inicial e a data final.
   * Imprime a lista de operações. */
   PROCEDURE pr_listar_operacoes (
-    id_setor SETOR.id_setor%TYPE,
-    data_inicial DATE,
-    data_final DATE
+    p_id_setor SETOR.id_setor%TYPE,
+    p_data_inicial DATE,
+    p_data_final DATE
   );
 
   /* Exceção lançada quando existe uma restrição de aplicação de fatores de produção. */
-  restricao_encontrada EXCEPTION;
+  e_restricao_encontrada EXCEPTION;
 END gestao_operacoes;
