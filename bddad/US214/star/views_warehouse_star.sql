e46467c2932933440336bc8bd16f629f4db2468f
André Barros <andrebarros0426@gmail.com>
2022-12-29 09:59:10 +0000

    feat: added missing views to star model

--- bddad/US214/star/views_warehouse_star.sql
+++ bddad/US214/star/views_warehouse_star.sql
@@ -5,5 +5,27 @@ CREATE OR REPLACE VIEW evolucaoProducaoUltimosCincoAnos AS
   INNER JOIN Tempo t ON pr.id_tempo = t.id_tempo
   INNER JOIN Setor s ON pr.id_setor = s.id_setor
   WHERE t.ano >= SYSDATE - 5 AND t.ano <= SYSDATE
-  GROUP BY p.tipo, t.ano, t.mes;
+  GROUP BY p.tipo, t.ano, t.mes
+  ORDER BY t.ano, t.mes;
+
+CREATE OR REPLACE VIEW compararVendasAnuais AS
+  SELECT p.tipo, t.ano, SUM(pr.quantidade) AS quantidade
+  FROM Venda ve
+  INNER JOIN Produto p ON ve.id_produto = p.id_produto
+  INNER JOIN Tempo t ON ve.id_tempo = t.id_tempo
+  INNER JOIN Setor s ON ve.id_setor = s.id_setor
+  WHERE t.ano >= SYSDATE - 1 AND t.ano <= SYSDATE
+  GROUP BY p.tipo, t.ano
+  ORDER BY t.ano;
+
+CREATE OR REPLACE VIEW compararVendasMensaisPorTipoCultura AS
+  SELECT p.tipo, t.mes, SUM(pr.quantidade) AS quantidade
+  FROM Venda ve
+  INNER JOIN Produto p ON ve.id_produto = p.id_produto
+  INNER JOIN Tempo t ON ve.id_tempo = t.id_tempo
+  INNER JOIN Setor s ON ve.id_setor = s.id_setor
+  WHERE t.mes >= SYSDATE - 1 AND t.mes <= SYSDATE
+  GROUP BY p.tipo, t.mes
+  ORDER BY t.mes;
+
 
