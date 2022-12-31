4de4a8456ecf9a00d3bca3c96b57e2b1f2ba38b3
André Barros <andrebarros0426@gmail.com>
2022-12-31 16:08:05 +0000

    chore: added role constants

--- src/main/java/isep/shared/Constants.java
+++ src/main/java/isep/shared/Constants.java
@@ -13,4 +13,12 @@ public class Constants {
   public static final int DEFAULT_IRRIGATION_PLAN_DURATION = 30;
 
   public static final String PARAMS_FILENAME = "config.properties";
+
+  public static final int MAX_OF_PASSWORD_TRIES = 3;
+
+  public static final String ROLE_GESTOR_AGRICOLA = "Gestor Agricola";
+  public static final String ROLE_CLIENTE = "Cliente";
+  public static final String ROLE_CONDUTOR = "Condutor";
+  public static final String ROLE_GESTOR_DISTRIBUICAO = "Gestor de destribuição";
+
 }
