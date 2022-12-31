3d673087561b7b4f81833f4f7b3c6b2b44c0e1f8
André Barros <andrebarros0426@gmail.com>
2022-12-31 15:39:48 +0000

    feat(UI): added Utils for UI development

---
+++ src/main/java/isep/ui/utils/Utils.java
@@ -0,0 +1,223 @@
+package isep.ui.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class Utils {
+
+  static public String readLineFromConsole(String prompt) {
+    try {
+      System.out.println("\n" + prompt);
+
+      InputStreamReader converter = new InputStreamReader(System.in);
+      BufferedReader in = new BufferedReader(converter);
+
+      return in.readLine();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  static public String readLineFromConsoleWithValidation(String prompt) {
+    boolean valid = false;
+    String answer = "";
+
+    do {
+      try {
+        System.out.println("\n" + prompt);
+
+        InputStreamReader converter = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(converter);
+
+        answer = in.readLine();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+      if (answer.length() > 0) {
+        valid = true;
+      } else {
+        System.out.println("Invalid input! Please try again.\n");
+      }
+
+    } while (!valid);
+
+    return answer;
+  }
+
+  static public int readIntegerFromConsole(String prompt) {
+    do {
+      try {
+        String input = readLineFromConsole(prompt);
+
+        int value = Integer.parseInt(input);
+
+        return value;
+      } catch (NumberFormatException ex) {
+        System.out.println("Invalid input! Please try again.\n");
+        // Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    } while (true);
+  }
+
+  static public int readPositiveIntegerFromConsole(String prompt) {
+    while (true) {
+      int n = readIntegerFromConsole(prompt);
+
+      if (n > 0)
+        return n;
+      else
+        System.out.println("\nThe value must be positive!");
+    }
+  }
+
+  static public int readNonNegativeIntegerFromConsole(String prompt) {
+    while (true) {
+      int n = readIntegerFromConsole(prompt);
+
+      if (n >= 0)
+        return n;
+      else
+        System.out.println("\nThe value cannot be negative!");
+    }
+  }
+
+  static public double readDoubleFromConsole(String prompt) {
+    do {
+      try {
+        String input = readLineFromConsole(prompt);
+
+        double value = Double.parseDouble(input);
+
+        return value;
+      } catch (NumberFormatException ex) {
+        System.out.println("Invalid input! Please try again.\n");
+        // Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    } while (true);
+  }
+
+  static public Date readDateFromConsole(String prompt) {
+    do {
+      try {
+        String strDate = readLineFromConsole(prompt);
+
+        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+        Date date = df.parse(strDate);
+
+        return date;
+      } catch (ParseException ex) {
+        System.out.println("Invalid input! Please try again.\n");
+        // Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    } while (true);
+  }
+
+  static public Date readDateInFutureFromConsole(String prompt) {
+    SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+    do {
+      Date date = readDateFromConsole(prompt);
+
+      try {
+        if (date.after(df.parse(df.format(new Date()))) || date.equals(df.parse(df.format(new Date()))))
+          return date;
+        System.out.println("The date must be in the future!\n");
+      } catch (ParseException e) {
+        System.out.println("Invalid input! Please try again.\n");
+      }
+    } while (true);
+  }
+
+  static public boolean confirm(String message) {
+    String input;
+    do {
+      input = Utils.readLineFromConsole("\n" + message + "\n");
+    } while (!input.equalsIgnoreCase("y") && !input.equalsIgnoreCase("n"));
+
+    return input.equalsIgnoreCase("y");
+  }
+
+  static public Object showAndSelectOne(List list, String header) {
+    showList(list, header);
+    return selectsObject(list);
+  }
+
+  static public int showAndSelectIndex(List list, String header) {
+    showList(list, header);
+    return selectsIndex(list);
+  }
+
+  static public Object showAndSelectOneEnum(Enum[] list, String header) {
+    showEnumItems(list, header);
+    return selectEnumObject(list);
+  }
+
+  static public void showList(List list, String header) {
+    System.out.println(header);
+
+    int index = 0;
+    for (Object o : list) {
+      index++;
+
+      System.out.println(index + ". " + o.toString());
+    }
+    System.out.println("");
+    System.out.println("0 - Cancel");
+  }
+
+  static public void showEnumItems(Enum[] items, String header) {
+    System.out.println(header);
+
+    int index = 0;
+    for (Enum o : items) {
+      index++;
+
+      System.out.println(index + ". " + o.toString());
+    }
+    System.out.println("");
+    System.out.println("0 - Cancel");
+  }
+
+  static public Object selectsObject(List list) {
+    int value = 0;
+
+    do {
+      value = Utils.readIntegerFromConsole("Type your option: ");
+    } while (value < 0 || value > list.size());
+
+    if (value == 0)
+      return null;
+    else
+      return list.get(value - 1);
+  }
+
+  static public Object selectEnumObject(Enum[] list) {
+    int value = 0;
+
+    do {
+      value = Utils.readIntegerFromConsole("Type your option: ");
+    } while (value < 0 || value > list.length);
+
+    if (value == 0)
+      return null;
+    else
+      return list[value - 1];
+  }
+
+  static public int selectsIndex(List list) {
+    int value = -1;
+    do {
+      value = Utils.readIntegerFromConsole("Type your option: ");
+    } while (value < 0 || value > list.size());
+
+    return value - 1;
+  }
+}
