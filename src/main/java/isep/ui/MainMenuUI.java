bd6aa3f122b37d629a5115398d41c49f5bcb2cee
André Barros <andrebarros0426@gmail.com>
2022-12-31 16:07:48 +0000

    feat(UI): added main menu and menu item

---
+++ src/main/java/isep/ui/MainMenuUI.java
@@ -0,0 +1,29 @@
+package isep.ui;
+
+import isep.ui.utils.Utils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainMenuUI {
+
+  public MainMenuUI() {
+  }
+
+  public void run() throws IOException {
+    List<MenuItem> options = new ArrayList<MenuItem>();
+    options.add(new MenuItem("Do Login", new AuthUI()));
+    options.add(new MenuItem("Know the Development Team", new DevTeamUI()));
+
+    int option = 0;
+    do {
+      option = Utils.showAndSelectIndex(options, "\n\nMain Menu");
+
+      if ((option >= 0) && (option < options.size())) {
+        options.get(option).run();
+      }
+    } while (option != -1);
+  }
+
+}
