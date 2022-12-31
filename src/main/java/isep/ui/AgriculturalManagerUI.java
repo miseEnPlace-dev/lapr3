60dcb8b791c78b6748f94f249de16abb60e9d720
André Barros <andrebarros0426@gmail.com>
2022-12-31 16:30:24 +0000

    feat(UI): added UI's for each role

---
+++ src/main/java/isep/ui/AgriculturalManagerUI.java
@@ -0,0 +1,10 @@
+package isep.ui;
+
+public class AgriculturalManagerUI implements Runnable {
+
+  @Override
+  public void run() {
+    // TODO Auto-generated method stub
+
+  }
+}
---
+++ src/main/java/isep/ui/DriverUI.java
@@ -0,0 +1,11 @@
+package isep.ui;
+
+public class DriverUI implements Runnable {
+
+  @Override
+  public void run() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
