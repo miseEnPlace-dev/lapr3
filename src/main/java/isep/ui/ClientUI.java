dd5457d34f51134087e9085b7605917d2c52715a
André Barros <andrebarros0426@gmail.com>
2023-01-01 18:50:27 +0000

    feat(UI): clientUI

--- src/main/java/isep/ui/ClientUI.java
+++ src/main/java/isep/ui/ClientUI.java
@@ -1,10 +1,19 @@
 package isep.ui;
 
+import isep.controller.App;
+import isep.model.Company;
+
 public class ClientUI implements Runnable {
+
+  public ClientUI() {
+    App app = App.getInstance();
+    Company company = app.getCompany();
+  }
 
   @Override
   public void run() {
-    // TODO Auto-generated method stub
+
+    System.out.println("\nNo action available for this role");
 
   }
 
