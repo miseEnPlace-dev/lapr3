c927b9e8d313da84e8abbd10ce1b53aef21e902f
André Barros <andrebarros0426@gmail.com>
2023-01-01 18:54:16 +0000

    feat(UI): agriculturalmanagerUI

--- src/main/java/isep/ui/AgriculturalManagerUI.java
+++ src/main/java/isep/ui/AgriculturalManagerUI.java
@@ -1,10 +1,53 @@
 package isep.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import isep.controller.App;
+import isep.controller.LoadDistributionNetworkController;
+import isep.model.DistributionNetwork;
+import isep.model.store.EntityStore;
+import isep.ui.utils.Utils;
+
 public class AgriculturalManagerUI implements Runnable {
+  LoadDistributionNetworkController loadDistributionNetworkController;
+  EntityStore entityStore;
+  DistributionNetwork distributionNetwork;
+
+  public AgriculturalManagerUI() {
+    entityStore = App.getInstance().getCompany().getEntityStore();
+    distributionNetwork = App.getInstance().getCompany().getDistributionNetwork();
+  }
 
   @Override
   public void run() {
-    // TODO Auto-generated method stub
+
+    List<MenuItem> options = new ArrayList<>();
+
+    options.add(new MenuItem("Load distribution network", new LoadDistributionNetworkUI()));
+    options.add(new MenuItem("Minimum number of connections", new GetMinNumberOfConnectionsUI()));
+    options.add(new MenuItem("Add hubs to network", new DefineHubsUI()));
+    options.add(new MenuItem("Find Nearest Hub", new NearestHubUI()));
+    options.add(new MenuItem("Find network minimum cost", new NetworkMinimumCostUI()));
+    // options.add(new MenuItem("Import basket list", new ImportBasketListUI()));
+    // options.add(new MenuItem("Generate expedition list", new
+    // GenerateExpeditionListUI()));
+    // options.add(new MenuItem("Generate expedition list with N producers", new
+    // GenerateExpeditionListNUI()));
+    // options.add(new MenuItem("Minimum total distance for expedition list", new
+    // MinimumTotalDistanceUI()));
+    // options.add(new MenuItem("Statistics", new StatisticsUI()));
+
+    int option = 0;
+
+    do {
+      option = Utils.showAndSelectIndex(options, "\n\nAgricultural Manager Menu:");
+
+      if ((option >= 0) && (option < options.size())) {
+        options.get(option).run();
+      }
+
+    } while (option != -1);
 
   }
 }
