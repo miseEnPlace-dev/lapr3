cc13c719695bedb7adb332a75604eab71279917b
Tomás Russo <tomas.russo4@gmail.com>
2023-01-03 17:45:55 +0000

    feat(us212): updated methods header at us212

--- bddad/US212/pkg_gerir_sensor.sql
+++ bddad/US212/pkg_gerir_sensor.sql
@@ -1,4 +1,9 @@
 CREATE OR REPLACE PACKAGE gerir_sensores AS
-  FUNCTION fn_NEsimoTuplo(n NUMBER) RETURN input_sensor.input_string%TYPE;
-  PROCEDURE pr_TransferirLeituras;
+  /** Permite obter o tuplo n da tabela InputSensor. */
+  FUNCTION fn_obter_tuplo (
+    p_n NUMBER
+  ) RETURN input_sensor.input_string%TYPE;
+
+  /** Permite atualizar a tabela Sensor, a partir da tabela InputSensor. */
+  PROCEDURE pr_atualizar_sensores;
 END gerir_sensores;
