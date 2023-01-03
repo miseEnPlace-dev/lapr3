895315b66570bdf72d19e12dc2949f75bf79e614
André Barros <andrebarros0426@gmail.com>
2023-01-03 19:11:29 +0000

    feat(UI): added new options to menu of agri manager

---
+++ src/main/java/isep/ui/GenerateExpeditionListUI.java
@@ -0,0 +1,14 @@
+package isep.ui;
+
+public class GenerateExpeditionListUI implements Runnable {
+
+  public GenerateExpeditionListUI() {
+  }
+
+  @Override
+  public void run() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
