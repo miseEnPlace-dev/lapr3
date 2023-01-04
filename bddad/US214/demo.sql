ba240e97e64535b3076787dcba7611d4766790fa
André Barros <andrebarros0426@gmail.com>
2023-01-04 22:37:01 +0000

    fix(db): views warehouse of production and sales & added demo

---
+++ bddad/US214/demo.sql
@@ -0,0 +1,8 @@
+-- 1º View
+SELECT * FROM evolucaoProducaoUltimosCincoAnosPorCulturaESetor;
+
+-- 2º View
+SELECT * FROM compararVendasAnuais;
+
+-- 3º View
+SELECT * FROM compararVendasMensaisPorTipoCultura;
