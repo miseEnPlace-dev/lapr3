af1f3591c667ed3440e84e88f122ece989518a27
André Barros <andrebarros0426@gmail.com>
2023-01-04 23:18:23 +0000

    chore: added missing inserts to facts tables

--- bddad/US216/star/star_insert.sql
+++ bddad/US216/star/star_insert.sql
@@ -48,21 +48,114 @@ INSERT INTO Hub VALUES (4, 'E2');
 INSERT INTO Hub VALUES (5, 'P3');
 
 -- Fact table --
-INSERT INTO Venda VALUES (1, 1, 1, 1, 1, 10);
-INSERT INTO Venda VALUES (2, 2, 2, 2, 2, 20);
-INSERT INTO Venda VALUES (3, 3, 3, 3, 3, 30);
-INSERT INTO Venda VALUES (4, 1, 4, 4, 4, 40);
-INSERT INTO Venda VALUES (5, 2, 5, 5, 5, 50);
-INSERT INTO Venda VALUES (6, 3, 6, 6, 6, 60);
-INSERT INTO Venda VALUES (7, 1, 7, 7, 7, 70);
-INSERT INTO Venda VALUES (8, 2, 8, 8, 8, 80);
+-- id_producao, id_produto, id_setor, id_tempo, id_hub, quantidade
+-- Setor 1 = Batatas, id_hub = 1
+INSERT INTO Producao VALUES (1, 1, 1, 1, 1, 100);
+INSERT INTO Producao VALUES (2, 1, 1, 2, 1, 200);
+INSERT INTO Producao VALUES (3, 1, 1, 3, 1, 300);
+INSERT INTO Producao VALUES (4, 1, 1, 4, 1, 400);
+INSERT INTO Producao VALUES (5, 1, 1, 5, 1, 500);
+-- Setor 2 = Cenouras, id_hub = 2
+INSERT INTO Producao VALUES (6, 2, 2, 1, 2, 600);
+INSERT INTO Producao VALUES (7, 2, 2, 2, 2, 700);
+INSERT INTO Producao VALUES (8, 2, 2, 3, 2, 800);
+INSERT INTO Producao VALUES (9, 2, 2, 4, 2, 900);
+INSERT INTO Producao VALUES (10, 2, 2, 5, 2, 1000);
+-- Setor 3 = Alfaces, id_hub = 3
+INSERT INTO Producao VALUES (11, 3, 3, 1, 3, 1100);
+INSERT INTO Producao VALUES (12, 3, 3, 2, 3, 1200);
+INSERT INTO Producao VALUES (13, 3, 3, 3, 3, 1300);
+INSERT INTO Producao VALUES (14, 3, 3, 4, 3, 1400);
+INSERT INTO Producao VALUES (15, 3, 3, 5, 3, 1500);
 
-INSERT INTO Producao VALUES (1, 1, 1, 1, 10);
-INSERT INTO Producao VALUES (2, 2, 2, 2, 20);
-INSERT INTO Producao VALUES (3, 3, 3, 3, 30);
-INSERT INTO Producao VALUES (4, 4, 1, 4, 40);
-INSERT INTO Producao VALUES (5, 5, 2, 5, 50);
-INSERT INTO Producao VALUES (6, 6, 3, 6, 60);
-INSERT INTO Producao VALUES (7, 7, 1, 7, 70);
-INSERT INTO Producao VALUES (8, 8, 2, 8, 80);
+-- Setor 4 = Tomates, id_hub = 4
+INSERT INTO Producao VALUES (16, 4, 4, 1, 4, 1600);
+INSERT INTO Producao VALUES (17, 4, 4, 2, 4, 1700);
+INSERT INTO Producao VALUES (18, 4, 4, 3, 4, 1800);
+INSERT INTO Producao VALUES (19, 4, 4, 4, 4, 1900);
+INSERT INTO Producao VALUES (20, 4, 4, 5, 4, 2000);
+
+-- Setor 5 = Maçãs, id_hub = 1
+INSERT INTO Producao VALUES (21, 5, 5, 1, 1, 2100);
+INSERT INTO Producao VALUES (22, 5, 5, 2, 1, 2200);
+INSERT INTO Producao VALUES (23, 5, 5, 3, 1, 2300);
+INSERT INTO Producao VALUES (24, 5, 5, 4, 1, 2400);
+INSERT INTO Producao VALUES (25, 5, 5, 5, 1, 2500);
+
+-- Setor 6 = Peras, id_hub = 2
+INSERT INTO Producao VALUES (26, 6, 6, 1, 2, 2600);
+INSERT INTO Producao VALUES (27, 6, 6, 2, 2, 2700);
+INSERT INTO Producao VALUES (28, 6, 6, 3, 2, 2800);
+INSERT INTO Producao VALUES (29, 6, 6, 4, 2, 2900);
+INSERT INTO Producao VALUES (30, 6, 6, 5, 2, 3000);
+-- Setor 7 = Laranjas, id_hub = 3
+INSERT INTO Producao VALUES (31, 7, 7, 1, 3, 3100);
+INSERT INTO Producao VALUES (32, 7, 7, 2, 3, 3200);
+INSERT INTO Producao VALUES (33, 7, 7, 3, 3, 3300);
+INSERT INTO Producao VALUES (34, 7, 7, 4, 3, 3400);
+INSERT INTO Producao VALUES (35, 7, 7, 5, 3, 3500);
+-- Setor 8 = Limões, id_hub = 5
+INSERT INTO Producao VALUES (36, 8, 8, 1, 5, 3600);
+INSERT INTO Producao VALUES (37, 8, 8, 2, 5, 3700);
+INSERT INTO Producao VALUES (38, 8, 8, 3, 5, 3800);
+INSERT INTO Producao VALUES (39, 8, 8, 4, 5, 3900);
+INSERT INTO Producao VALUES (40, 8, 8, 5, 5, 4000);
+
+
+-- id_venda, id_client, id_produto, id_setor, id_tempo, id_hub, quantidade
+-- Setor 1 = Batatas, id_hub = 1
+INSERT INTO Venda VALUES (1, 1, 1, 1, 1, 1, 100);
+INSERT INTO Venda VALUES (2, 1, 1, 1, 2, 1, 200);
+INSERT INTO Venda VALUES (3, 1, 1, 1, 3, 1, 300);
+INSERT INTO Venda VALUES (4, 1, 1, 1, 4, 1, 400);
+INSERT INTO Venda VALUES (5, 1, 1, 1, 5, 1, 500);
+
+-- Setor 2 = Cenouras, id_hub = 2
+INSERT INTO Venda VALUES (6, 2, 2, 2, 1, 2, 600);
+INSERT INTO Venda VALUES (7, 2, 2, 2, 2, 2, 700);
+INSERT INTO Venda VALUES (8, 2, 2, 2, 3, 2, 800);
+INSERT INTO Venda VALUES (9, 2, 2, 2, 4, 2, 900);
+INSERT INTO Venda VALUES (10, 2, 2, 2, 5, 2, 1000);
+
+-- Setor 3 = Alfaces, id_hub = 3
+INSERT INTO Venda VALUES (11, 3, 3, 3, 1, 3, 1100);
+INSERT INTO Venda VALUES (12, 3, 3, 3, 2, 3, 1200);
+INSERT INTO Venda VALUES (13, 3, 3, 3, 3, 3, 1300);
+INSERT INTO Venda VALUES (14, 3, 3, 3, 4, 3, 1400);
+INSERT INTO Venda VALUES (15, 3, 3, 3, 5, 3, 1500);
+
+-- Setor 4 = Tomates, id_hub = 4
+INSERT INTO Venda VALUES (16, 1, 4, 4, 1, 4, 1600);
+INSERT INTO Venda VALUES (17, 1, 4, 4, 2, 4, 1700);
+INSERT INTO Venda VALUES (18, 1, 4, 4, 3, 4, 1800);
+INSERT INTO Venda VALUES (19, 1, 4, 4, 4, 4, 1900);
+INSERT INTO Venda VALUES (20, 1, 4, 4, 5, 4, 2000);
+
+-- Setor 5 = Maçãs, id_hub = 1
+INSERT INTO Venda VALUES (21, 2, 5, 5, 1, 1, 2100);
+INSERT INTO Venda VALUES (22, 2, 5, 5, 2, 1, 2200);
+INSERT INTO Venda VALUES (23, 2, 5, 5, 3, 1, 2300);
+INSERT INTO Venda VALUES (24, 2, 5, 5, 4, 1, 2400);
+INSERT INTO Venda VALUES (25, 2, 5, 5, 5, 1, 2500);
+
+-- Setor 6 = Peras, id_hub = 2
+INSERT INTO Venda VALUES (26, 3, 6, 6, 1, 2, 2600);
+INSERT INTO Venda VALUES (27, 3, 6, 6, 2, 2, 2700);
+INSERT INTO Venda VALUES (28, 3, 6, 6, 3, 2, 2800);
+INSERT INTO Venda VALUES (29, 3, 6, 6, 4, 2, 2900);
+INSERT INTO Venda VALUES (30, 3, 6, 6, 5, 2, 3000);
+
+-- Setor 7 = Laranjas, id_hub = 3
+INSERT INTO Venda VALUES (31, 1, 7, 7, 1, 3, 3100);
+INSERT INTO Venda VALUES (32, 1, 7, 7, 2, 3, 3200);
+INSERT INTO Venda VALUES (33, 1, 7, 7, 3, 3, 3300);
+INSERT INTO Venda VALUES (34, 1, 7, 7, 4, 3, 3400);
+INSERT INTO Venda VALUES (35, 1, 7, 7, 5, 3, 3500);
+
+-- Setor 8 = Limões, id_hub = 5
+INSERT INTO Venda VALUES (36, 2, 8, 8, 1, 5, 3600);
+INSERT INTO Venda VALUES (37, 2, 8, 8, 2, 5, 3700);
+INSERT INTO Venda VALUES (38, 2, 8, 8, 3, 5, 3800);
+INSERT INTO Venda VALUES (39, 2, 8, 8, 4, 5, 3900);
+INSERT INTO Venda VALUES (40, 2, 8, 8, 5, 5, 4000);
 
