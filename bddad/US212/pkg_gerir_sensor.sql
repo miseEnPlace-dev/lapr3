ab631d3f23bfbd7e87611f6f3b34cb671e4cede0
Tomás Russo <tomas.russo4@gmail.com>
2023-01-06 15:54:50 +0000

    feat(us212): updated pkg gerir_sensores

--- bddad/US212/pkg_gerir_sensor.sql
+++ bddad/US212/pkg_gerir_sensor.sql
@@ -1,12 +1,33 @@
 CREATE OR REPLACE PACKAGE gerir_sensores AS
+  /** A input_string da tabela InputSensor tem o seguinte formato:
+  *  IIIIIHHMMMVVVYYYYMMDDHHMM
+  *
+  *  identificador: 5 caracteres (IIIII)
+  *  tipo: 2 caracteres (TT)
+  *  medicao: 3 caracteres (MMM)
+  *  valor_referencia: 3 caracteres (VVV)
+  *  data_hora: 12 caracteres (YYYYMMDDHHMM)
+  *
+  *  Exemplo: 12345HS070090202301051544
+  */
+
   /** Permite obter o tuplo n da tabela InputSensor. */
   FUNCTION fn_obter_tuplo (
     p_n NUMBER
-  ) RETURN input_sensor.input_string%TYPE;
+  ) RETURN INPUTSENSOR.string%TYPE;
 
   /** Permite atualizar a tabela Sensor, a partir da tabela InputSensor. */
   PROCEDURE pr_atualizar_sensores;
 
-  /** Exceção lançada quando o identificador do sensor não existe na tabela Sensor. */
-  EXCEPTION sensor_inexistente;
+  /** Permite registar um erro na tabela ErroInputSensor. 
+  * Se o sensor não existir, é inserido um registo com o identificador do sensor a 0.
+  */
+  PROCEDURE pr_regista_erro (
+    p_id_leitura LEITURAINPUTSENSOR.id_leitura%TYPE,
+    p_identificador SENSOR.identificador%TYPE
+  );
+
+  PROCEDURE pr_imprime_informacao_leitura (
+    p_id_leitura LEITURAINPUTSENSOR.id_leitura%TYPE
+  );
 END gerir_sensores;
