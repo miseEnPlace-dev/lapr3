344b2883dc9815544260c54e04486e6903b4cd8f
Ricardo M <rmoreiradev@gmail.com>
2023-01-07 20:45:38 +0000

    feat: added menu to build irrigation ui

--- src/main/java/isep/ui/BuildIrrigationCtrlUI.java
+++ src/main/java/isep/ui/BuildIrrigationCtrlUI.java
@@ -1,13 +1,12 @@
 package isep.ui;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.naming.NameNotFoundException;
 import isep.controller.BuildIrrigationCtrlController;
 import isep.model.IrrigationController;
 import isep.model.IrrigationPlan;
-import isep.shared.exceptions.InvalidFileFormatException;
-import isep.shared.exceptions.InvalidHourFormatException;
 import isep.ui.utils.Utils;
 
 public class BuildIrrigationCtrlUI implements Runnable {
@@ -18,6 +17,34 @@ public class BuildIrrigationCtrlUI implements Runnable {
 
   @Override
   public void run() {
+    List<String> options = new ArrayList<>();
+    options.add("Build irrigation controller from a file");
+    options.add("Insert mock parcels");
+    options.add("Get irrigation status from the controller");
+
+    int option = 0;
+
+    do {
+      option = Utils.showAndSelectIndex(options, "\nBuild Irrigation Controller Menu:");
+
+      if ((option >= 0) && (option < options.size())) {
+        switch (option) {
+          case 0:
+            buildIrrigationCtrl();
+            break;
+          case 1:
+            insertMockParcels();
+            break;
+          case 2:
+            getIrrigationStatus();
+            break;
+        }
+        Utils.readLineFromConsole("\nPress any key to continue... ");
+      }
+    } while (option != -1);
+  }
+
+  private void buildIrrigationCtrl() {
     String filePath = Utils.readLineFromConsole("\nInsert the path of the file containing an irrigation plan: ");
 
     try {
@@ -26,11 +53,21 @@ public class BuildIrrigationCtrlUI implements Runnable {
       IrrigationPlan plan = ctrl.mapIrrigationPlanData(data);
       irrigationCtrl = ctrl.createIrrigationController(plan);
       System.out.println("Irrigation controller created successfully.");
+
+
     } catch (FileNotFoundException e) {
       System.out.println("The file does not exist.");
-    } catch (NameNotFoundException | InvalidHourFormatException | InvalidFileFormatException e) {
+    } catch (NameNotFoundException e) {
+      System.out.println("Error: " + e.getMessage());
+      System.out.println("Perhaps you may want to load mock parcels first.");
+    } catch (Exception e) {
       System.out.println("Error: " + e.getMessage());
     }
   }
 
+  private void insertMockParcels() {}
+
+  private void getIrrigationStatus() {
+
+  }
 }
