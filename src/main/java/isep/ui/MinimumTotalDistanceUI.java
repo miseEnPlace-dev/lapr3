b2adbbf777b5417b6f15cffe171c1aa8d365495f
Tomás Russo <tomas.russo4@gmail.com>
2023-01-07 17:17:20 +0000

    chore: ui refinement

--- src/main/java/isep/ui/MinimumTotalDistanceUI.java
+++ src/main/java/isep/ui/MinimumTotalDistanceUI.java
@@ -19,6 +19,12 @@ public class MinimumTotalDistanceUI implements Runnable {
   public void run() {
     expeditionList = App.getInstance().getCompany().getCurrentExpeditionList();
 
+    if (expeditionList == null) {
+      System.out.println("\nPlease load an expedition list first...");
+      System.out.println("Going back to main menu...");
+      return;
+    }
+
     network = App.getInstance().getCompany().getDistributionNetwork();
     controller = new ExpeditionPathController(network, expeditionList);
 
@@ -26,6 +32,5 @@ public class MinimumTotalDistanceUI implements Runnable {
 
     System.out.println("");
     expeditionPath.printPath();
-
   }
 }
