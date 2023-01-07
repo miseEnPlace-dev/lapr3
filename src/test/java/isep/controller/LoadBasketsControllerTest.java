a8abf9da4fc08d429712a3f30d7381ea1eab687e
Ricardo M <rmoreiradev@gmail.com>
2023-01-07 23:30:22 +0000

    tests: added tests to the basket loader ctrl

--- src/test/java/isep/controller/LoadBasketsControllerTest.java
+++ src/test/java/isep/controller/LoadBasketsControllerTest.java
@@ -1,8 +1,15 @@
 package isep.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import isep.mock.EntityStoreMock;
 import isep.model.Company;
+import isep.model.store.EntityStore;
 
 public class LoadBasketsControllerTest {
   private static final String BASKETS_TEST_FILEPATH = "src/test/resources/cabazesSmall.csv";
@@ -15,7 +22,19 @@ public class LoadBasketsControllerTest {
   }
 
   @Test
-  public void test() {
+  public void testWrongFile() {
+    ctrl.setFilename("wrong.csv");
+    assertThrows(FileNotFoundException.class, () -> {
+      ctrl.readData();
+    });
+  }
 
+  @Test
+  public void testMapBaskets() throws FileNotFoundException {
+    EntityStore store = new EntityStoreMock().mockEntityStoreWithSmallFile();
+    ctrl.setFilename(BASKETS_TEST_FILEPATH);
+    List<Map<String, String>> data = ctrl.readData();
+    int count = ctrl.mapBaskets(data, store);
+    assertEquals(62, count);
   }
 }
